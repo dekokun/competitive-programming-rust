7de62cb00a4690829ec4342bfb06d260a914ba55
dekokun <shintaro.kurachi@gmail.com>
2019-05-25 22:52:46 +0900

    save

--- abc127_d/src/main.rs
+++ abc127_d/src/main.rs
@@ -17,14 +17,19 @@ fn main() {
     let N: i32 = read();
     let M: i32 = read();
     let mut cards: Vec<i32> = vec![];
+    let mut replaces: Vec<(i32, i32)> = vec![];
 
     for _ in 0..N {
         cards.push(read());
     }
+    cards.sort();
 
     for _ in 0..M {
         let b: i32 = read();
         let c: i32 = read();
+        replaces.push((b, c));
     }
+    replaces.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+    println!("{:?}", replaces);
     println!("{}", 14);
 }
