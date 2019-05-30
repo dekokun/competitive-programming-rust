14e46c437394c1914e0e58a2aedfcb07b012071c
dekokun <shintaro.kurachi@gmail.com>
2019-05-30 13:53:27 +0900

    new problem template

---
+++ new_problem.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo 'Argument is not exists.'
+    exit 1
+fi
+
+program_name="$1"
+cargo new abc127_e --bin
+cp -pr template/src/main.rs "$program_name"/src
+cp -pr template/runtest "$program_name"/runtest
