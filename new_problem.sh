ca7ec71684686ef7d295fe8cfbbadab8d9e05f77
dekokun <dekokun@hatena.ne.jp>
2019-06-09 21:05:57 +0900

    runtestはファイル

--- new_problem.sh
+++ new_problem.sh
@@ -8,6 +8,5 @@ fi
 program_name="$1"
 cargo new "$program_name" --bin
 mkdir "$program_name"/src
-mkdir "$program_name"/runtest
 cp -pr template/src/main.rs "$program_name"/src
 cp -pr template/runtest "$program_name"/runtest
