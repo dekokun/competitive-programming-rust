e03ad48ddd03e4a8db22b82235211dcf36cf5b23
dekokun <dekokun@hatena.ne.jp>
2019-06-09 21:01:56 +0900

    make dir

--- new_problem.sh
+++ new_problem.sh
@@ -7,5 +7,7 @@ fi
 
 program_name="$1"
 cargo new abc127_e --bin
+mkdir "$program_name"/src
+mkdir "$program_name"/runtest
 cp -pr template/src/main.rs "$program_name"/src
 cp -pr template/runtest "$program_name"/runtest
