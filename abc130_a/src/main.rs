ae21cb99aa973c4e88f68136ce2915132f1920de
dekokun <dekokun@hatena.ne.jp>
2019-06-16 21:04:31 +0900

    pass abc130_a

---
+++ abc130_a/src/main.rs
@@ -0,0 +1,24 @@
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
+fn main() {
+    let x: i32 = read();
+    let a: i32 = read();
+    if x < a {
+        println!("{}", 0);
+    } else {
+        println!("{}", 10);
+    }
+}
