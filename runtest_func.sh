d1602eab3133a8a1890c369b981df70e23a0b249
dekokun <dekokun@hatena.ne.jp>
2019-06-16 20:59:35 +0900

    colored ouutput

--- runtest_func.sh
+++ runtest_func.sh
@@ -1,15 +1,19 @@
 # compile check
 cargo build || exit 1
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 runtest() {
   output=$(echo "$1" | cargo run -q)
   if [ "$output" != "$2" ]; then
-    echo fail
+    echo -e "${RED}fail"
     echo "input: $1"
     echo
-    echo "$2 expected. but got $output"
+    echo -e "$2 expected. but got $output${NC}"
   else
-    echo OK
+    echo -e "${GREEN}OK${NC}"
   fi
 }
 
