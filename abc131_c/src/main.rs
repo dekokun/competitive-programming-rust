05a76cae2731a0cced7f92423a913c3610a0af5a
dekokun <dekokun@hatena.ne.jp>
2019-06-23 20:54:11 +0900

    maybe pass abc131_c

---
+++ abc131_c/src/main.rs
@@ -0,0 +1,44 @@
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    let mut a = a;
+    let mut b = b;
+    if a < b {
+        std::mem::swap(&mut a, &mut b);
+    }
+    if b == 0 {
+        return a;
+    }
+    gcd(b, a % b)
+}
+fn lcm(a: u64, b: u64) -> u64 {
+    let gcd = gcd(a, b);
+    (a / gcd) * b
+}
+
+fn main() {
+    let a: u64 = read();
+    let b: u64 = read();
+    let c: u64 = read();
+    let d: u64 = read();
+    let all_count = b - a + 1;
+    let c_count = (b / c) - ((a - 1) / c);
+    let d_count = (b / d) - ((a - 1) / d);
+
+    let lcm = lcm(c, d);
+    let lcm_count = (b / lcm) - (a / lcm);
+    println!("{}", all_count - c_count - d_count + lcm_count);
+}
