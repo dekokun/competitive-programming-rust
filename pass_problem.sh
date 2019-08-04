84b22a06a902dc27d972aee210741a3d81c5aed8
dekokun <dekokun@hatena.ne.jp>
2019-08-04 20:41:47 +0900

    add merge when pass script

---
+++ pass_problem.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+program_name=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$program_name" == "master" ]; then
+    echo "Current branch is master. This program is merge branch that name is program name to master."
+    exit 1;
+fi
+
+git add "$program_name"/*
+git commit -m"$program_name pass"
+git checkuot master
+git merge "$program_name"
