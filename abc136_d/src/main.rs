8d5e2c3cedb0cdf8bcfe0ed3dc7b8a0db8040425
dekokun <dekokun@hatena.ne.jp>
2019-08-04 22:33:10 +0900

    abc136_d pass

--- abc136_d/src/main.rs
+++ abc136_d/src/main.rs
@@ -14,4 +14,70 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
+    let s: String = read();
+    let mut gather_indexes = vec![];
+    let mut before = 'R';
+    for (i, c) in s.chars().enumerate().skip(1) {
+        if before == 'R' && c == 'L' {
+            gather_indexes.push(i - 1);
+        }
+        before = c;
+    }
+    let mut gather_index = 0;
+    let mut ans = vec![0; s.len()];
+    for (i, c) in s.chars().enumerate() {
+        // dbg!(s.clone(), c, i, gather_index, gather_indexes.clone());
+        if c == 'L' {
+            if (std::cmp::max(gather_indexes[gather_index], i)
+                - std::cmp::min(gather_indexes[gather_index], i))
+                % 2
+                == 0
+            {
+                ans[gather_indexes[gather_index]] += 1;
+            } else {
+                ans[gather_indexes[gather_index] + 1] += 1;
+            }
+        } else {
+            if gather_indexes[gather_index] == i {
+                ans[gather_indexes[gather_index]] += 1;
+            // dbg!("1");
+            } else if gather_indexes[gather_index] > i {
+                if (std::cmp::max(gather_indexes[gather_index], i)
+                    - std::cmp::min(gather_indexes[gather_index], i))
+                    % 2
+                    == 0
+                {
+                    ans[gather_indexes[gather_index]] += 1;
+                // dbg!("2");
+                } else {
+                    ans[gather_indexes[gather_index] + 1] += 1;
+                    // dbg!("3");
+                }
+            } else if (std::cmp::max(gather_indexes[gather_index + 1], i)
+                - std::cmp::min(gather_indexes[gather_index + 1], i))
+                % 2
+                == 0
+            {
+                ans[gather_indexes[gather_index + 1]] += 1;
+            // dbg!("2");
+            } else {
+                ans[gather_indexes[gather_index + 1] + 1] += 1;
+                // dbg!("3");
+            }
+        }
+        // dbg!(ans.clone());
+        if gather_index == gather_indexes.len() - 1 {
+            continue;
+        }
+        if gather_indexes[gather_index + 1] == i + 1 {
+            gather_index += 1;
+        }
+    }
+    println!(
+        "{}",
+        ans.iter()
+            .map(|c| c.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
+    );
 }
