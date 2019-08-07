7290633f6816eecaa8d1e3767b13a84b1be30f26
dekokun <dekokun@hatena.ne.jp>
2019-08-07 21:56:17 +0900

    abc128_c pass

--- abc128_c/src/main.rs
+++ abc128_c/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::*;
 use std::str::FromStr;
 
@@ -14,4 +15,51 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
+    // switch num
+    let switch_num: i32 = read();
+    // bulb num
+    let bulb_num: i32 = read();
+    let mut map = HashMap::new();
+    // bi: bulb index
+    for bi in 1..bulb_num + 1 {
+        let k: i32 = read();
+        for _ in 1..k + 1 {
+            // si: switch index
+            let si: i32 = read();
+            let entry = map.entry(si).or_insert(vec![]);
+            entry.push(bi)
+        }
+    }
+    let mut vec = vec![true];
+    for _ in 1..bulb_num + 1 {
+        vec.push(read::<i32>() == 0);
+    }
+    println!("{}", dfs(1, switch_num, &map, vec, 0));
+}
+
+fn dfs(
+    depth: i32,
+    last: i32,
+    map: &HashMap<i32, Vec<i32>>,
+    switch_state: Vec<bool>,
+    ans: i32,
+) -> i32 {
+    if depth == last + 1 {
+        if switch_state.iter().all(|&x| x == true) {
+            return ans + 1;
+        }
+        return ans;
+    }
+    let mut switch_state_flip = switch_state.clone();
+    match map.get(&depth) {
+        Some(vec) => {
+            for &i in vec {
+                switch_state_flip[i as usize] ^= true;
+            }
+        }
+        None => {}
+    };
+    let ans1 = dfs(depth + 1, last, &map, switch_state_flip, ans);
+    let ans2 = dfs(depth + 1, last, &map, switch_state, ans);
+    ans1 + ans2
 }
