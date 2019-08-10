576d397af5717ad69342870685e6c1e4b30614e8
dekokun <dekokun@hatena.ne.jp>
2019-08-10 22:53:55 +0900

    tmp commit

--- abc137_d/src/main.rs
+++ abc137_d/src/main.rs
@@ -14,4 +14,30 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
+    let n: i32 = read();
+    let m: i32 = read();
+    let mut rewards = vec![];
+    let mut set = std::collections::HashMap::new();
+    for _ in 0..n {
+        let reward = read::<i32>();
+        let days = read::<i32>();
+        set.entry(reward).or_insert(vec![]).push(days);
+        rewards.push(reward);
+    }
+    let mut map = std::collections::HashMap::new();
+    for reward in &rewards {
+        let mut dayss = set.remove(reward).unwrap();
+        dayss.sort();
+        dayss.reverse();
+        map.insert(reward.clone(), dayss);
+    }
+    rewards.sort();
+    rewards.reverse();
+    let mut total_reward = 0;
+    for i in 1..m {
+        for reward in &rewards {
+            for days in map[reward].clone() {}
+        }
+    }
+    println!("{}", total_reward);
 }
