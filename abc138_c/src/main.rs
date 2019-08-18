af78b32f02ad0aaa20b420ea367888b1f15c02c8
dekokun <dekokun@hatena.ne.jp>
2019-08-18 21:28:11 +0900

    abc138_c pass

--- abc138_c/src/main.rs
+++ abc138_c/src/main.rs
@@ -14,4 +14,19 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
+    let n: i8 = read();
+    let mut vec = vec![];
+    for _ in 0..n {
+        let v: f64 = read();
+        vec.push(v);
+    }
+    //reverse order
+    vec.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    for _ in 0..(n - 1) {
+        let v1 = vec.pop().unwrap();
+        let v2 = vec.pop().unwrap();
+        vec.push((v1 + v2) / 2.0);
+        vec.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    }
+    println!("{}", vec.pop().unwrap());
 }
