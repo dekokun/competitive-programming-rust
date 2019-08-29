8ee385fd3e73b69f7fbc3f24e7d8394d2b1b5801
dekokun <dekokun@hatena.ne.jp>
2019-08-29 21:38:02 +0900

    arc045_a pass

--- arc045_a/src/main.rs
+++ arc045_a/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::*;
 use std::str::FromStr;
 
-fn read<T: FromStr>() -> T {
+fn readOption<T: FromStr>() -> Option<T> {
     let stdin = stdin();
     let stdin = stdin.lock();
     let token: String = stdin
@@ -10,8 +10,36 @@ fn read<T: FromStr>() -> T {
         .skip_while(|c| c.is_whitespace())
         .take_while(|c| !c.is_whitespace())
         .collect();
-    token.parse().ok().expect("failed to parse token")
+    token.parse().ok()
+}
+
+fn read<T: FromStr>() -> T {
+    let opt = readOption();
+    opt.expect("failed to parse token")
+}
+
+fn read2() -> Option<String> {
+    let opt = readOption();
+    match opt {
+        Some(s) => {
+            if s != "" {
+                Some(s)
+            } else {
+                None
+            }
+        }
+        None => None,
+    }
 }
 
 fn main() {
+    let mut vec = vec![];
+    let mut map = std::collections::HashMap::new();
+    map.insert("Left".to_string(), "<");
+    map.insert("Right".to_string(), ">");
+    map.insert("AtCoder".to_string(), "A");
+    while let Some(s) = read2() {
+        vec.push(map[&s]);
+    }
+    println!("{}", vec.join(" "));
 }
