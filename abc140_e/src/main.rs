7411b8eb8781cd26978b9954ef50838e158a1f1b
dekokun <dekokun@hatena.ne.jp>
2019-09-13 21:52:53 +0900

    maybe pass
    compiler is old

--- abc140_e/src/main.rs
+++ abc140_e/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeSet;
 use std::io::*;
 use std::str::FromStr;
 
@@ -21,30 +22,57 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
-    let n: usize = read();
+    // index: i64, value: usize
+    let n: i64 = read();
     // let mut vec = vec![0; n];
     let mut p: Vec<usize> = vec![];
-    let mut sum = 0;
-    let mut max;
-    let mut second;
+    let mut rev: Vec<usize> = vec![0; n as usize];
     for _ in 0..n {
-        p.push(read());
+        p.push(read::<usize>() - 1);
     }
-    for i in 0..n {
-        for j in i + 1..n {
-            max = 0;
-            second = 0;
-            for k in i..j + 1 {
-                // vec[k] += 1;
-                if max < p[k] {
-                    second = max;
-                    max = p[k];
-                } else if second < p[k] {
-                    second = p[k];
-                }
+    for (i, &v) in p.iter().enumerate() {
+        rev[v] = i;
+    }
+    let mut sum: i64 = 0;
+    let mut set = BTreeSet::new();
+    // 上から埋めていく
+    for i in (0..n).rev() {
+        let index = rev[i as usize] as i64;
+
+        set.insert(index as i64);
+        // 重要。端に達した場合はそこまで数える
+        let mut l = vec![-1; 2];
+        let mut r = vec![n; 2];
+        let mut before = set.range(..index).next_back();
+        for j in 0..2 {
+            if before.is_none() {
+                break;
             }
-            sum += second;
+            l[j] = *before.unwrap();
+            before = set.range(..l[j]).next_back();
         }
+        // rangeは左端は含む半開区間
+        let mut after = set.range((index + 1)..).next();
+        for j in 0..2 {
+            if after.is_none() {
+                break;
+            }
+            r[j] = *after.unwrap();
+            after = set.range((r[j] + 1)..).next();
+        }
+        let mut ls = vec![0; 2];
+        let mut rs = vec![0; 2];
+        ls[0] = index - l[0];
+        ls[1] = l[0] - l[1];
+        rs[0] = r[0] - index;
+        rs[1] = r[1] - r[0];
+        let point = ls[0] * rs[1] + ls[1] * rs[0];
+        if i == n - 1 {
+            assert_eq!(0, point);
+        } else {
+            assert!(point >= 0, "point is negative");
+        }
+        sum += point * (i + 1);
     }
     println!("{}", sum);
 }
