980c25641dd0f415a6b41b3f07e40d5913a790e6
dekokun <dekokun@hatena.ne.jp>
2019-09-23 21:46:02 +0900

    add random test

---
+++ agc038_a/src/bin/make_test.rs
@@ -0,0 +1,72 @@
+use rand::prelude::*;
+// h w a b
+fn make_random_vec() -> (usize, usize, usize, usize, Vec<Vec<usize>>) {
+    let mut rng = rand::thread_rng();
+    let h = rng.gen_range(1, 10);
+    let w = rng.gen_range(1, 10);
+    dbg!(h, w);
+    let mut vec = vec![vec![0; w]; h];
+    'outer: loop {
+        // randomize
+        for _ in 0..1000 {
+            let a = rng.gen_range(0, w);
+            let b = rng.gen_range(0, h);
+            if vec[b][a] == 0 {
+                vec[b][a] = 1;
+            } else {
+                vec[b][a] = 0;
+            }
+        }
+        // a check
+        let mut before_a = None;
+        for row in vec.iter() {
+            let mut a_count = 0;
+            for v in row.iter() {
+                if *v == 1 {
+                    a_count += 1;
+                }
+            }
+            let val = std::cmp::min(a_count, w - a_count);
+            if before_a.is_some() && val != before_a.unwrap() {
+                continue 'outer;
+            }
+            before_a = Some(val);
+        }
+        // b check
+        let mut b_counts = vec![0; w];
+        for row in vec.iter() {
+            for (i, v) in row.iter().enumerate() {
+                if *v == 1 {
+                    b_counts[i] += 1;
+                }
+            }
+        }
+        let mut before_b = None;
+        for v in b_counts {
+            let val = std::cmp::min(v, h - v);
+            if before_b.is_some() && val != before_b.unwrap() {
+                continue 'outer;
+            }
+            before_b = Some(val);
+        }
+        return (h, w, before_a.unwrap(), before_b.unwrap(), vec);
+    }
+}
+fn main() {
+    let (h, w, a, b, vec) = make_random_vec();
+    println!(
+        "t \"{} {} {} {}\" \"{}\"",
+        h,
+        w,
+        a,
+        b,
+        vec.iter()
+            .map(|row| row
+                .iter()
+                .map(|a| a.to_string())
+                .collect::<Vec<_>>()
+                .join(""))
+            .collect::<Vec<_>>()
+            .join("\n")
+    )
+}
--- agc038_a/src/main.rs
+++ agc038_a/src/main.rs
@@ -21,4 +21,82 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
+    let h: usize = read();
+    let w: usize = read();
+    let a: usize = read();
+    let b: usize = read();
+    let mut vec = vec![vec![0; w]; h];
+    if a == 0 {
+        for i in 0..b {
+            let row = &mut vec[i];
+            for j in 0..w {
+                row[j] = 1;
+            }
+        }
+    }
+    if b == 0 {
+        for row in vec.iter_mut() {
+            for i in 0..a {
+                row[i] = 1;
+            }
+        }
+    } else {
+        for row in vec.iter_mut() {
+            for i in 0..a {
+                row[i] = 1;
+            }
+        }
+        let mut plus_column = a;
+        let mut count = 0;
+        for i in b..h {
+            for j in 0..a {
+                if count == b {
+                    plus_column += a;
+                }
+                if j + plus_column >= w {
+                    continue;
+                }
+                vec[i].swap(j, j + plus_column);
+                count += 1;
+            }
+        }
+    }
+    // a check
+    for row in vec.iter() {
+        let mut a_count = 0;
+        for v in row.iter() {
+            if *v == 1 {
+                a_count += 1;
+            }
+        }
+        if std::cmp::min(a_count, w - a_count) != a {
+            println!("-1");
+            return;
+        }
+    }
+    // b check
+    let mut b_counts = vec![0; w];
+    for row in vec.iter() {
+        for (i, v) in row.iter().enumerate() {
+            if *v == 1 {
+                b_counts[i] += 1;
+            }
+        }
+    }
+    for v in b_counts {
+        if std::cmp::min(v, h - v) != b {
+            println!("-1");
+            return;
+        }
+    }
+
+    for row in vec.iter() {
+        println!(
+            "{}",
+            row.iter()
+                .map(|a| a.to_string())
+                .collect::<Vec<_>>()
+                .join("")
+        )
+    }
 }
