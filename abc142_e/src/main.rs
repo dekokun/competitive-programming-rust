f37711e61f7276618a4194be5513bb2b745d7f14
dekokun <dekokun@hatena.ne.jp>
2019-09-28 23:11:31 +0900

    wrong implement

--- abc142_e/src/main.rs
+++ abc142_e/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::*;
 use std::str::FromStr;
 
@@ -21,4 +22,35 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
+    let n: usize = read();
+    let m: usize = read();
+    let mut map = HashMap::new();
+    for _ in 0..m {
+        let a: usize = read();
+        let b: usize = read();
+        for _ in 0..b {
+            let c: usize = read();
+            map.entry(c).or_insert(vec![]).push(a);
+        }
+    }
+    let mut ans = 0;
+    for i in 1..n + 1 {
+        let val = map.get(&i);
+        if val.is_none() {
+            dbg!(i, val);
+            println!("-1");
+            return;
+        }
+        let val = val.unwrap();
+        if val.len() == 0 {
+            println!("-1");
+            return;
+        }
+        let mut min = 1000000000;
+        for v in val {
+            min = std::cmp::min(min, *v);
+        }
+        ans += min;
+    }
+    println!("{}", ans);
 }
