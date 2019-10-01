7851a35a5571c1bdfc05cbedf162384636104fda
dekokun <dekokun@hatena.ne.jp>
2019-10-01 21:38:34 +0900

    dp_e pass

--- dp_e/src/main.rs
+++ dp_e/src/main.rs
@@ -21,4 +21,34 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
+    let n: usize = read();
+    let w: usize = read();
+    const INF: i64 = std::i64::MAX / 3;
+    let v_max: usize = 1000 * n + 1;
+    // (weight, value)
+    let mut vec: Vec<(i64, i64)> = vec![];
+    for _ in 0..n {
+        vec.push((read(), read()));
+    }
+    let mut dp: Vec<Vec<i64>> = vec![vec![INF; v_max + 1]; n + 1];
+    dp[0][0] = 0;
+    for i in 0..n {
+        for j in 0..v_max {
+            if j < vec[i].1 as usize {
+                dp[i + 1][j] = dp[i][j];
+                continue;
+            }
+            dp[i + 1][j] = std::cmp::min(dp[i][j - vec[i].1 as usize] + vec[i].0, dp[i][j])
+        }
+    }
+    let mut max_val = 0;
+    for v1 in dp {
+        for (i, &v2) in v1.iter().enumerate() {
+            if v2 <= w as i64 {
+                max_val = std::cmp::max(max_val, i);
+            }
+        }
+    }
+
+    println!("{}", max_val);
 }
