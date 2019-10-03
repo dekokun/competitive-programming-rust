7af083ffadc597ef37396d444c2f42f5c63b1a1a
dekokun <dekokun@hatena.ne.jp>
2019-10-03 20:43:19 +0900

    dp_f pass

--- dp_f/src/main.rs
+++ dp_f/src/main.rs
@@ -35,5 +35,20 @@ fn main() {
             };
         }
     }
-    dbg!(dp[s.len()][t.len()]);
+    let mut res = vec![];
+    let mut i = s.len();
+    let mut j = t.len();
+    while i > 0 && j > 0 {
+        if dp[i][j] == dp[i - 1][j] {
+            i -= 1; // DP の遷移を遡る
+        } else if dp[i][j] == dp[i][j - 1] {
+            j -= 1;
+        } else {
+            res.push(s[i - 1]);
+            i -= 1;
+            j -= 1;
+        }
+    }
+    res.reverse();
+    println!("{}", res.into_iter().collect::<String>());
 }
