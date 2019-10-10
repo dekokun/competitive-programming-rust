fd0011660141d254e65bc1f678f3296526281ee0
dekokun <dekokun@hatena.ne.jp>
2019-10-10 22:19:54 +0900

    dp_h pass

--- dp_h/src/main.rs
+++ dp_h/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(non_snake_case)]
 use std::io::*;
 use std::str::FromStr;
 
@@ -21,4 +22,37 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
+    let MOD: i64 = 10_i64.pow(9) + 7;
+    let h: usize = read();
+    let w: usize = read();
+    let mut map: Vec<Vec<Option<i64>>> = vec![ vec![None; w]; h ];
+    for i in 0..h {
+        let str: String = read();
+        for (j, c) in str.chars().into_iter().enumerate() {
+            map[i][j] = match c {
+                '.' => Some(0),
+                '#' => None,
+                _ => panic!("strange char")
+            };
+        }
+    }
+    map[0][0] = Some(1);
+    for i in 0..h {
+        for j in 0..w {
+            if map[i][j].is_none() || (i == 0 && j == 0) {
+                continue;
+            }
+
+            map[i][j] = if (i == 0 || map[i - 1][j].is_none()) && (j == 0 || map[i][j - 1].is_none()) {
+                Some(0)
+            } else if i == 0 || map[i - 1][j].is_none() {
+                map[i][j - 1]
+            } else if j == 0 || map[i][j - 1].is_none() {
+                map[i - 1][j]
+            } else {
+                Some((map[i - 1][j].unwrap() + map[i][j - 1].unwrap()) % MOD)
+            };
+        }
+    }
+    println!("{}", map[h - 1][w - 1].unwrap());
 }
