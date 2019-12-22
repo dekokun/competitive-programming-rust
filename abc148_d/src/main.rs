38a8795f64f97a1707eee9f02a2772cc0856e743
dekokun <dekokun@hatena.ne.jp>
2019-12-22 21:40:06 +0900

    abc148_d pass

--- abc148_d/src/main.rs
+++ abc148_d/src/main.rs
@@ -1,4 +1,38 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
+    let N: usize = read();
+    let mut vec: Vec<usize> = vec![];
+    for _ in 0..N {
+        vec.push(read());
+    }
+    let mut tmp = 0;
+    for i in vec {
+        if i == tmp + 1 {
+            tmp += 1;
+        }
+    }
+    if tmp == 0 {
+        println!("-1");
+    } else {
+        println!("{}", N - tmp);
+    }
 }
