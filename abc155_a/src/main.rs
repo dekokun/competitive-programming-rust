9da8ad937717023b0fe9c0bf1a2b094e3fd80b08
dekokun <dekokun@hatena.ne.jp>
2020-02-16 21:06:38 +0900

    abc155_a pass

--- abc155_a/src/main.rs
+++ abc155_a/src/main.rs
@@ -1,4 +1,32 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
+    let A: u8 = read();
+    let B: u8 = read();
+    let C: u8 = read();
+    let mut vec = vec![A, B, C];
+    vec.sort();
+    if (A == B && B == C && A == C) || (A != B && B != C && C != A) {
+        println!("No");
+    } else {
+        println!("Yes");
+    }
 }
