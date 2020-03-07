716001f3031b8a3506f60b6603d7f31de1f1260b
dekokun <dekokun@hatena.ne.jp>
2020-03-07 21:10:59 +0900

    abc158_b pass

--- abc158_b/src/main.rs
+++ abc158_b/src/main.rs
@@ -1,4 +1,32 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
+    let N: u64 = read();
+    let A: u64 = read();
+    let B: u64 = read();
+    let rem = N % (A + B);
+    let add = if rem > A {
+        A
+    } else {
+        rem
+    };
+    println!("{}", ((N / (A + B)) * A) + add);
 }
