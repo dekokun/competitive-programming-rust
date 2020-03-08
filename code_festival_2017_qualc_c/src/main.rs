8edc3cf72393a2e601fda5b7a7cedd463b051896
dekokun <dekokun@hatena.ne.jp>
2020-03-08 15:35:07 +0900

    code_festival_2017_qualc_c pass

--- code_festival_2017_qualc_c/src/main.rs
+++ code_festival_2017_qualc_c/src/main.rs
@@ -22,4 +22,31 @@ fn main() {
     let S: String = read();
     // xを除いて回文じゃなかったら-1
     // 回文だったらある文字とある文字の間にxが何個あるかを回文と比較
+    let non_x = S.replace("x", "");
+    let non_x_rev: String = non_x.chars().rev().collect();
+    if non_x != non_x_rev {
+        println!("-1");
+        return;
+    }
+    // 計算の都合上xを前後に足しておく
+    let S = "x".to_string() + &S + "x";
+    let chars: Vec<_> = S.chars().collect();
+    let mut i = 0;
+    let count = S.chars().count();
+    let mut x_counts = vec![];
+    while i < count {
+        let mut skip_count = 0;
+        while i < count && chars[i] == 'x' {
+            skip_count += 1;
+            i += 1;
+        }
+        x_counts.push(skip_count);
+        i += 1;
+    }
+    let x_counts_rev: Vec<_> = x_counts.iter().rev().collect();
+    let mut ans = 0;
+    for (i, v) in x_counts.iter().enumerate() {
+        ans += std::cmp::max(v, x_counts_rev[i]) - std::cmp::min(v, x_counts_rev[i]);
+    }
+    println!("{}", ans / 2);
 }
