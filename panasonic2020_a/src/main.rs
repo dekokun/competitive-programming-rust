d668e2c0fa0676ff7e3874d57fc40af8b4cb4c7f
dekokun <dekokun@hatena.ne.jp>
2020-03-14 21:05:40 +0900

    panasonic2020_a pass

--- panasonic2020_a/src/main.rs
+++ panasonic2020_a/src/main.rs
@@ -1,4 +1,25 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
+    let K: usize = read();
+    let vec = [1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51];
+    println!("{}", vec[K - 1]);
 }
