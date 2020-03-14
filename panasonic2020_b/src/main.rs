57b2c444fb7b5cad12aec1ff1539c5d88304303b
dekokun <dekokun@hatena.ne.jp>
2020-03-14 21:14:21 +0900

    panasonic2020_b pass

--- panasonic2020_b/src/main.rs
+++ panasonic2020_b/src/main.rs
@@ -1,4 +1,29 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
+    let H: u64 = read();
+    let W: u64 = read();
+    if H == 1 || W == 1 {
+        println!("1");
+    } else {
+        println!("{}", (H * W + 1) / 2);
+    }
 }
