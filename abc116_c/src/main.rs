78f593030b67c772ac97d72cf6449df26ca8608e
dekokun <dekokun@hatena.ne.jp>
2020-03-24 08:34:34 +0900

    abc116_c pass

--- abc116_c/src/main.rs
+++ abc116_c/src/main.rs
@@ -1,4 +1,31 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
+    let N: usize = read();
+    let mut ans = 0;
+    let mut before = 0;
+    for _ in 0..N {
+        let h: i32 = read();
+        ans += std::cmp::max(h - before, 0);
+        before = h;
+    }
+    println!("{}", ans);
 }
