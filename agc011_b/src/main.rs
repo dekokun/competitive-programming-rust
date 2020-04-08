32da7752dd094165cea24f2f6c0f01d1f2d396fd
dekokun <dekokun@hatena.ne.jp>
2020-04-08 11:33:08 +0900

    agc011_b pass

--- agc011_b/src/main.rs
+++ agc011_b/src/main.rs
@@ -1,4 +1,37 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
+    let N: usize = read();
+    let mut vec: Vec<usize> = vec![];
+    for _ in 0..N {
+        vec.push(read());
+    }
+    vec.sort();
+    let mut sum = 0;
+    let mut min_index = 0;
+    for (i, &v) in vec.iter().enumerate() {
+        if sum * 2 < v {
+            min_index = i;
+        }
+        sum += v;
+    }
+    println!("{}", N - min_index);
 }
