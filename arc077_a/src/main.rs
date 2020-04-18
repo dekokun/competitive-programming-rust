99a8c8f943eddb2bb609f708322f7e2f4f6ba78e
dekokun <dekokun@hatena.ne.jp>
2020-04-18 21:34:46 +0900

    arc077_a pass

--- arc077_a/src/main.rs
+++ arc077_a/src/main.rs
@@ -1,4 +1,44 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
+    use std::collections::VecDeque;
+    let mut vecdeque: VecDeque<usize> = VecDeque::new();
+    let n: usize = read();
+    for i in 0..n {
+        if i % 2 == 0 {
+            vecdeque.push_back(read());
+        } else {
+            vecdeque.push_front(read());
+        }
+    }
+    let vec: Vec<usize> = if n % 2 == 1 {
+        vecdeque.into_iter().rev().collect()
+    } else {
+        vecdeque.into_iter().collect()
+    };
+    println!(
+        "{}",
+        vec.into_iter()
+            .map(|i| i.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
+    );
 }
