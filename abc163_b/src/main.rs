751d622e06d75caf7a3081ef0884c09e539325e0
dekokun <dekokun@hatena.ne.jp>
2020-04-19 21:11:51 +0900

    abc163_b pass

--- abc163_b/src/main.rs
+++ abc163_b/src/main.rs
@@ -1,4 +1,31 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
+    let N: usize = read();
+    let M: usize = read();
+    let work_day = (0..M).map(|_| read::<usize>()).sum::<usize>();
+    let ans = if work_day > N {
+        -1
+    } else {
+        (N - work_day) as i32
+    };
+    println!("{}", ans);
 }
