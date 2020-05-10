9ad584732cb5d824a3ae4720caf8b6746e0eea88
dekokun <dekokun@hatena.ne.jp>
2020-05-10 22:09:24 +0900

    local test pass

--- abc167_d/src/main.rs
+++ abc167_d/src/main.rs
@@ -1,18 +1,46 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-fn solve() {
-
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let N: usize = read();
+    let K: i64 = read();
+    let vec: Vec<usize> = (0..N).map(|_| read::<usize>() - 1).collect();
+    use std::collections::HashMap;
+    let mut set = HashMap::new();
+    let mut now = 0;
+    let mut vec2 = vec![];
+    vec2.push(now);
+    set.insert(now, 0);
+    loop {
+        now = vec[now];
+        match set.get(&now) {
+            None => {
+                vec2.push(now);
+                set.insert(now, vec2.len() - 1);
+            }
+            Some(_) => break,
+        }
     }
+    assert_eq!(set.len(), vec2.len());
+    let loop_first_idx = set.get(&now).unwrap();
+    let rem = K % (set.len() - loop_first_idx) as i64;
+    dbg!(now, loop_first_idx, &vec2, rem);
+    println!("{}", vec2[rem as usize] + 1);
 }
