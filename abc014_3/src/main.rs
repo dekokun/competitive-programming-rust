3c15f023da2e85d55be0472c343d9fb8ad81a329
dekokun <dekokun@hatena.ne.jp>
2020-05-13 09:43:33 +0900

    abc014_3 initialize

---
+++ abc014_3/src/main.rs
@@ -0,0 +1,18 @@
+#![allow(non_snake_case)]
+
+fn main() {
+}
+
+fn solve() {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(true, true);
+    }
+}
