55f2a7706f6f6b9327f88a845f8f5ae3b63e2f60
dekokun <dekokun@hatena.ne.jp>
2020-05-19 20:59:12 +0900

    abc014_2 pass

--- abc014_2/src/main.rs
+++ abc014_2/src/main.rs
@@ -1,17 +1,38 @@
 #![allow(non_snake_case)]
 
-fn main() {}
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
 
-#[allow(dead_code)]
-fn solve() {}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+fn main() {
+    let n: usize = read();
+    let X: usize = read();
+    let mut vec: Vec<usize> = vec![];
+    for _ in 0..n {
+        vec.push(read());
     }
+    let str = format!("{:b}", X);
+    let mut ans = 0;
+    for (i, c) in str.chars().rev().enumerate() {
+        match c {
+            '1' => ans += vec[i],
+            '0' => continue,
+            _ => unimplemented!(),
+        }
+    }
+    println!("{}", ans);
 }
