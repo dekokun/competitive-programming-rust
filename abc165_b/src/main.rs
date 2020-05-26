9a71f06d9131e63197365d501a1b1b8696c51bab
dekokun <dekokun@hatena.ne.jp>
2020-05-26 18:40:37 +0900

    abc165_b pass

--- abc165_b/src/main.rs
+++ abc165_b/src/main.rs
@@ -1,20 +1,30 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let X: i64 = read();
+    let mut now: f64 = 100.0;
+    let mut ans = 0;
+    while now < X as f64 {
+        ans += 1;
+        now = (now * 1.01).floor();
     }
+    println!("{}", ans);
 }
