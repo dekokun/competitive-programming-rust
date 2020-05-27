4936de0737b3ae789631c541fe0de34276a3b3e5
dekokun <dekokun@hatena.ne.jp>
2020-05-27 16:48:43 +0900

    arc075_a pass

--- arc075_a/src/main.rs
+++ arc075_a/src/main.rs
@@ -1,17 +1,36 @@
 #![allow(non_snake_case)]
 
-fn main() {}
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
 
-#[allow(dead_code)]
-fn solve() {}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
-    }
+fn main() {
+    let N: usize = read();
+    let vec: Vec<usize> = (0..N).map(|_| read()).collect();
+    let rems: Vec<_> = vec.iter().filter(|&x| x % 10 != 0).collect();
+    let sum = vec.iter().sum::<usize>();
+    let ans = if rems.is_empty() {
+        0
+    } else if sum % 10 != 0 {
+        sum
+    } else {
+        let mut rems = rems;
+        rems.sort();
+        sum - rems[0]
+    };
+    println!("{}", ans);
 }
