501270775f3b52599f3b1036e2f8aa7cd5a19a55
dekokun <dekokun@hatena.ne.jp>
2020-05-30 18:05:46 +0900

    arc065_a pass

--- arc065_a/src/main.rs
+++ arc065_a/src/main.rs
@@ -1,20 +1,41 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let S: String = read();
+    let mut vec: Vec<_> = S.chars().collect();
+    let mut now_string = "".to_string();
+    while !vec.is_empty() {
+        let c = vec.pop().unwrap();
+        now_string = now_string + &c.to_string();
+        match now_string.as_str() {
+            "maerd" | "remaerd" | "esare" | "resare" => {
+                now_string = "".to_string();
+            }
+            _ => {
+                if now_string.len() >= 7 {
+                    println!("NO");
+                    return;
+                }
+            }
+        }
     }
+    println!("YES");
 }
