2d3c8801344a64bd389341cdeada76d93e5d4f07
dekokun <dekokun@hatena.ne.jp>
2020-06-14 21:11:39 +0900

    abc170_b pass

--- abc170_b/src/main.rs
+++ abc170_b/src/main.rs
@@ -1,20 +1,29 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let x: usize = read();
+    let y: usize = read();
+    if x * 2 > y || y > x * 4 || y %2 == 1{
+        println!("No");
+    } else {
+        println!("Yes");
     }
 }
