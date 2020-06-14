d5fbed2df2702273c9e7d7e47ab01c97e145be1a
dekokun <dekokun@hatena.ne.jp>
2020-06-14 21:18:08 +0900

    abc170_c pass

--- abc170_c/src/main.rs
+++ abc170_c/src/main.rs
@@ -1,20 +1,35 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let x: i32 = read();
+    let n: usize = read();
+    let vec: Vec<i32> = (0..n).map(|_| read()).collect();
+    for i in 0..102 {
+        if !vec.contains(&(x - i)) {
+            println!("{}", x - i);
+            return;
+        }
+        if !vec.contains(&(x + i)) {
+            println!("{}", x + i);
+            return;
+        }
     }
 }
