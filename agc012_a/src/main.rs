6ba287368d594e55836e3c72bf9265acae3e1c8f
dekokun <dekokun@hatena.ne.jp>
2020-08-14 20:30:31 +0900

    agc012_a pass

--- agc012_a/src/main.rs
+++ agc012_a/src/main.rs
@@ -1,20 +1,33 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let N: usize = read();
+    let mut a: Vec<usize> = (0..3 * N).map(|_| read()).collect();
+    a.sort();
+    let mut sum = 0;
+    for i in (N..).step_by(2) {
+        if i >= a.len() {
+            break;
+        }
+        sum += a[i];
     }
+    println!("{}", sum);
 }
