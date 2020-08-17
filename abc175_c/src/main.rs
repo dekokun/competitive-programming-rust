ac07a0446fe3b84fafba1deeacc719b93b2909e4
dekokun <dekokun@hatena.ne.jp>
2020-08-17 13:47:33 +0900

    abc175_c pass

--- abc175_c/src/main.rs
+++ abc175_c/src/main.rs
@@ -1,20 +1,37 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let X: i64 = read();
+    let K: i64 = read();
+    let D: i64 = read();
+    let X = if X < 0 { -X } else { X };
+    let rem = X % D;
+    let div = X / D;
+    if div > K {
+        println!("{}", X - K * D);
+        return;
+    }
+    if div % 2 == K % 2 {
+        println!("{}", rem);
+    } else {
+        println!("{}", D - rem);
     }
 }
