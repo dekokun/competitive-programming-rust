290f736f7f93b147a44e74cb7267f7aa827d4dd7
dekokun <dekokun@hatena.ne.jp>
2020-08-24 17:31:35 +0900

    arc017_2 pass

--- arc017_2/src/main.rs
+++ arc017_2/src/main.rs
@@ -1,20 +1,40 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let N: usize = read();
+    let K: usize = read();
+    let mut before = 0;
+    let mut count = 0;
+    let mut ans = 0;
+    for _ in 0..N {
+        let a: usize = read();
+        if a <= before {
+            count = 1;
+        } else {
+            count += 1;
+        }
+        before = a;
+        if count >= K {
+            ans += 1;
+        }
     }
+    println!("{}", ans)
 }
