77a2898dc783dc32c2396ad03cf2f9ed4ce3cc19
dekokun <dekokun@hatena.ne.jp>
2020-08-27 19:03:39 +0900

    abc174_a pass

--- abc174_a/src/main.rs
+++ abc174_a/src/main.rs
@@ -1,20 +1,28 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
-    }
+    let x: isize = read();
+    println!("{}", if x >= 30 {
+        "Yes"
+    } else {
+        "No"
+    });
 }
