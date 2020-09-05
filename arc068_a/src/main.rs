bfa573a4d0870bd0a534482d970b05b196aa0c3b
dekokun <dekokun@hatena.ne.jp>
2020-09-05 15:46:01 +0900

    arc068_a pass

--- arc068_a/src/main.rs
+++ arc068_a/src/main.rs
@@ -1,20 +1,34 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
-    }
+    let x: u64 = read();
+    let ans = (x / 11) * 2;
+    println!(
+        "{}",
+        if x % 11 == 0 {
+            ans
+        } else if x % 11 > 6 {
+            ans + 2
+        } else {
+            ans + 1
+        }
+    );
 }
