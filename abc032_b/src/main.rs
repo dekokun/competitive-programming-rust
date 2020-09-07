10763d2ad387ed3531887712399d92659f26558f
dekokun <dekokun@hatena.ne.jp>
2020-09-07 17:49:29 +0900

    abc032_b pass

--- abc032_b/src/main.rs
+++ abc032_b/src/main.rs
@@ -1,20 +1,35 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let s: String = read();
+    let k: usize = read();
+    if k > s.len() {
+        println!("0");
+        return;
     }
+    use std::collections::HashSet;
+    let mut set: HashSet<String> = HashSet::new();
+    for s2 in s.chars().collect::<Vec<_>>().windows(k) {
+        let s2: String = s2.into_iter().collect();
+        set.insert(s2);
+    }
+    println!("{}", set.len());
 }
