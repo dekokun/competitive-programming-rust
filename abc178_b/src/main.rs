54faf41974ca933da062242c96f882caaa56cda8
dekokun <dekokun@hatena.ne.jp>
2020-09-13 21:16:17 +0900

    abc178_b pass

--- abc178_b/src/main.rs
+++ abc178_b/src/main.rs
@@ -1,20 +1,29 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
+use std::cmp::max;
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
-    }
+    let a: i64 = read();
+    let b: i64 = read();
+    let c: i64 = read();
+    let d: i64 = read();
+    let ans = max(max(a * c, a * d), max(b * c, b * d));
+    println!("{}", ans);
 }
