71e9f8a048d66cdea418d9ef1a30aca9c212ff92
dekokun <dekokun@hatena.ne.jp>
2020-10-03 21:09:19 +0900

    maybe pass

--- arc104_a/src/main.rs
+++ arc104_a/src/main.rs
@@ -1,20 +1,26 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
-    }
+    let (a, b): (i32, i32) = (read(), read());
+    let x = (a + b) / 2;
+    let y = (a - b) / 2;
+    println!("{} {}", x, y)
 }
