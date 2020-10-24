d30faf9184e7fe8b2da1a35c7113eecdb10984f4
dekokun <dekokun@hatena.ne.jp>
2020-10-24 20:50:39 +0900

    abc016_1 pass

--- abc016_1/src/main.rs
+++ abc016_1/src/main.rs
@@ -1,11 +1,35 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
+    let m: usize = read();
+    let d: usize = read();
+    println!("{}", if m %d == 0 {
+        "YES"
+    } else {
+        "NO"
+    });
 }
 
 #[allow(dead_code)]
-fn solve() {
-}
+fn solve() {}
 
 #[cfg(test)]
 #[allow(unused_imports)]
