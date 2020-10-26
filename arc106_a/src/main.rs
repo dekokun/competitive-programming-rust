d429e2a7879de34368f216c3b2bc332908fbe709
dekokun <dekokun@hatena.ne.jp>
2020-10-26 13:53:54 +0900

    arc106_a pass

--- arc106_a/src/main.rs
+++ arc106_a/src/main.rs
@@ -1,20 +1,47 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let n: u64 = read();
+    let mut fives = vec![5];
+    while fives[fives.len() - 1] <= n / 5 {
+        fives.push(fives[fives.len() - 1] * 5);
     }
+    for (i, v) in fives.into_iter().enumerate() {
+        if (n - v) % 3 != 0 {
+            continue;
+        }
+        let mut third_pow = 1;
+        let mut three = 0;
+        while third_pow <= n - v {
+            // dbg!(v, n - v, third_pow);
+            if third_pow == (n - v) {
+                println!("{} {}", three, i + 1);
+                return;
+            }
+            three += 1;
+            if third_pow > std::u64::MAX / 3 {
+                break;
+            }
+            third_pow *= 3;
+        }
+    }
+    println!("-1");
 }
