c47386638b9bd166c9acf67525ea6ee9a7a9b47e
dekokun <dekokun@hatena.ne.jp>
2020-12-01 09:47:11 +0900

    arc108_b pass

--- arc108_b/src/main.rs
+++ arc108_b/src/main.rs
@@ -1,20 +1,37 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let _n: usize = read();
+    let s: String = read();
+    let mut vec = vec![];
+    'outer: for c in s.chars() {
+        vec.push(c);
+        if c == 'x' && vec.len() >= 3 {
+            if vec[vec.len() - 1] == 'x' && vec[vec.len() - 2] == 'o' && vec[vec.len() - 3] == 'f' {
+                vec.pop();
+                vec.pop();
+                vec.pop();
+            }
+        }
     }
+    // dbg!(&vec);
+    println!("{}", vec.len());
 }
