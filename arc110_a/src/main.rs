367f02ac7379737cc047a395826076fecea42eaa
dekokun <dekokun@hatena.ne.jp>
2020-12-05 21:16:08 +0900

    arc110_a pass

--- arc110_a/src/main.rs
+++ arc110_a/src/main.rs
@@ -1,20 +1,61 @@
 #![allow(non_snake_case)]
 
-fn main() {
+use std::io::{stdin, Read};
+use std::str::FromStr;
+use std::collections::HashMap;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
 }
 
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+fn prime_factorization(n: usize) -> HashMap<usize, usize> {
+    let mut ans = HashMap::new();
+    let mut n = n;
+    for i in 2..=((n as f64).sqrt().ceil() as usize) {
+        while n % i == 0 && n != 1 {
+            n /= i;
+            let entry = ans.entry(i).or_insert(0);
+            *entry += 1;
+        }
     }
+    if n != 1 {
+        let entry = ans.entry(n).or_insert(0);
+        *entry += 1;
+    }
+    ans
+}
+
+fn main() {
+    let n: usize = read();
+
+    let mut divisors: HashMap<usize, usize> = HashMap::new();
+    for i in 1..=n {
+        let primes = prime_factorization(i);
+        for (key, v) in primes {
+            match divisors.get(&key) {
+                Some(&v2) => {
+                    divisors.insert(key, v.max(v2));
+                },
+                None => {
+                    divisors.insert(key, v);
+                }
+            }
+        }
+    }
+    let mut ans = 1;
+    for (key, v) in divisors {
+        ans *= key.pow(v as u32) as u64;
+    }
+    println!("{}", ans + 1)
 }
