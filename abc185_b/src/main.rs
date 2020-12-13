1b6dec28ab7b1cac01ad1868b582f2c65ca3bf8b
dekokun <dekokun@hatena.ne.jp>
2020-12-13 21:11:27 +0900

    abc185_b pass

--- abc185_b/src/main.rs
+++ abc185_b/src/main.rs
@@ -1,20 +1,44 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let n: i64 = read();
+    let m: i64 = read();
+    let t: i64 = read();
+    let mut now: i64 = n;
+    let mut before = 0;
+    for _ in 0..m {
+        let a: i64 = read();
+        let b: i64 = read();
+        now -= a - before;
+        if now <= 0 {
+            println!("No");
+            return;
+        }
+        now = std::cmp::min(n, now + (b - a));
+        before = b;
     }
+    now -= t - before;
+    if now <= 0 {
+        println!("No");
+        return;
+    }
+    println!("Yes")
 }
