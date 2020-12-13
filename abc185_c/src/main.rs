13c6eee39606389be5a618ff70007d927e41bffc
dekokun <dekokun@hatena.ne.jp>
2020-12-13 21:21:18 +0900

    abc185_c pass

--- abc185_c/src/main.rs
+++ abc185_c/src/main.rs
@@ -1,20 +1,40 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
+
 fn main() {
-}
+    let n: u64 = read();
+    // n-1 C 11 を求める
+    let mut ans = 1;
+    use std::collections::HashSet;
+    let mut set: HashSet<u64> = (1..=11).collect();
 
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    for i in 0..11 {
+        ans *= n - 1 - i;
+        for v in set.clone() {
+            if ans % v == 0 {
+                ans /= v;
+                set.remove(&v);
+            }
+        }
     }
+
+    println!("{}", ans)
 }
