76a665065936704c2a84755c2496007f1daec8ce
dekokun <dekokun@hatena.ne.jp>
2020-12-17 15:28:52 +0900

    caddi2018_a pass

--- caddi2018_a/src/main.rs
+++ caddi2018_a/src/main.rs
@@ -1,20 +1,52 @@
 #![allow(non_snake_case)]
 
-fn main() {
+use std::collections::HashMap;
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
 }
 
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+fn prime_factorization(n: u64) -> HashMap<u64, u64> {
+    let mut ans = HashMap::new();
+    let mut n = n;
+    for i in 2..=((n as f64).sqrt().ceil() as usize) {
+        let i = i as u64;
+        while n % i == 0 && n != 1 {
+            n /= i;
+            let entry = ans.entry(i).or_insert(0);
+            *entry += 1;
+        }
     }
+    if n != 1 {
+        let entry = ans.entry(n).or_insert(0);
+        *entry += 1;
+    }
+    ans
+}
+
+fn main() {
+    let n: u64 = read();
+    let p: u64 = read();
+
+    let primes = prime_factorization(p);
+    let mut ans = 1;
+    for (key, v) in primes {
+        if v >= n {
+            ans *= key.pow((v / n) as u32);
+        }
+    }
+    println!("{}", ans)
 }
