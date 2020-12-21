93945628826abe53bafd2f06b34d5895bddb111a
dekokun <dekokun@hatena.ne.jp>
2020-12-21 21:07:16 +0900

    abc183_d pass

--- abc183_d/src/main.rs
+++ abc183_d/src/main.rs
@@ -1,20 +1,41 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let n: i64 = read();
+    let w: i64 = read();
+    let mut imos = vec![0; 2 * 10_usize.pow(5) + 1];
+    for _ in 0..n {
+        let s: i64 = read();
+        let t: i64 = read();
+        let p: i64 = read();
+        imos[s as usize] += p;
+        imos[t as usize] += -p;
     }
+    let mut now = 0;
+    for v in imos {
+        now += v;
+        if now > w {
+            println!("No");
+            return
+        }
+    }
+    println!("Yes");
 }
