ed72887d3d6e2f4483bc81db104c8417dba93c40
dekokun <dekokun@hatena.ne.jp>
2021-01-02 21:18:03 +0900

    abc187_d initialize

---
+++ abc187_d/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(non_snake_case)]
+
+fn main() {
+}
+
+#[allow(dead_code)]
+fn solve() {
+}
+
+#[cfg(test)]
+#[allow(unused_imports)]
+mod tests {
+    #![allow(unused_imports)]
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(true, true);
+    }
+}
