9a50a562613e74395ce31faac876df488eeb2bf3
dekokun <dekokun@hatena.ne.jp>
2021-01-06 09:53:59 +0900

    abc056_a pass

--- abc056_a/src/main.rs
+++ abc056_a/src/main.rs
@@ -1,20 +1,35 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-}
-
-#[allow(dead_code)]
-fn solve() {
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
-    }
+    let a: char = read();
+    let b: char = read();
+    println!(
+        "{}",
+        match (a, b) {
+            ('H', 'H') | ('D', 'D') => {
+                'H'
+            }
+            _ => {
+                'D'
+            }
+        }
+    );
 }
