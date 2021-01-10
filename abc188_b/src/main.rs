d2fec6994be9fa7ebf517191b434c0de976f12c3
dekokun <dekokun@hatena.ne.jp>
2021-01-10 21:03:56 +0900

    abc188_b initialize

---
+++ abc188_b/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(non_snake_case)]
+
+fn main() {
+    println!("{}", solve(read(), ));
+}
+
+fn solve(n: usize) -> usize {
+}
+
+#[cfg(test)]
+#[allow(unused_imports)]
+mod tests {
+    #![allow(unused_imports)]
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(true, true);
+    }
+}
