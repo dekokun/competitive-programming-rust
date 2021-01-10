b8621159657999fcb62f3b6e34543cf389f30642
dekokun <dekokun@hatena.ne.jp>
2021-01-10 21:03:52 +0900

    abc188_a pass

--- abc188_a/src/main.rs
+++ abc188_a/src/main.rs
@@ -1,10 +1,33 @@
 #![allow(non_snake_case)]
 
-fn main() {
-    println!("{}", solve(read(), ));
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
 }
 
-fn solve(n: usize) -> usize {
+fn main() {
+    println!("{}", solve(read(), read()));
+}
+
+fn solve(x: usize, y: usize) -> String {
+    if x.min(y) + 3 > x.max(y) {
+        "Yes".into()
+    } else {
+        "No".into()
+    }
 }
 
 #[cfg(test)]
