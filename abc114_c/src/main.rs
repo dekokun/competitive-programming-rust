f260dd0dbc8aa64e48cdb4c6b67785d0879e81fd
dekokun <dekokun@hatena.ne.jp>
2021-01-14 13:54:45 +0900

    abc114_c pass

--- abc114_c/src/main.rs
+++ abc114_c/src/main.rs
@@ -1,20 +1,50 @@
 #![allow(non_snake_case)]
 
+use std::{
+    collections::HashSet,
+    io::{stdin, Read},
+};
+use std::{collections::VecDeque, str::FromStr};
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-    println!("{}", solve(read(), ));
+    println!("{}", solve(read()));
 }
 
 fn solve(n: usize) -> usize {
-}
+    let mut queue = VecDeque::new();
+    queue.push_front(0);
+    let mut ans = 0;
+    while let Some(v) = queue.pop_back() {
+        if v > n {
+            continue;
+        }
 
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+        let mut tmp = HashSet::new();
+        for c in v.to_string().chars() {
+            tmp.insert(c);
+        }
+        if tmp.len() == 3 {
+            ans += 1;
+        }
+        for &new in &[7, 5, 3] {
+            let next = v * 10 + new;
+            queue.push_front(next);
+        }
     }
+    ans
 }
