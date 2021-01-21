a90e33b1410ad633895c4eaef51ab259d320ae12
dekokun <dekokun@hatena.ne.jp>
2021-01-21 09:23:06 +0900

    abc009_2 pass

--- abc009_2/src/main.rs
+++ abc009_2/src/main.rs
@@ -1,20 +1,37 @@
 #![allow(non_snake_case)]
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
-    println!("{}", solve(read(), ));
+    let n: usize = read();
+    println!("{}", solve((0..n).map(|_| read()).collect()));
 }
 
-fn solve(n: usize) -> usize {
-}
+fn solve(mut a: Vec<usize>) -> usize {
+    a.sort();
+    a.reverse();
 
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test1() {
-        assert_eq!(true, true);
+    let max = a[0];
+    for v in a {
+        if v != max {
+            return v;
+        }
     }
+    panic!()
 }
