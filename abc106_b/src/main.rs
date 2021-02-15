a66afc29e97af5f3dc8a2e4f030d737b114157c9
dekokun <dekokun@hatena.ne.jp>
2021-02-15 13:50:03 +0900

    abc106_b pass

--- abc106_b/src/main.rs
+++ abc106_b/src/main.rs
@@ -31,4 +31,24 @@ fn main() {
     println!("{}", solve(n));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(n: usize) -> usize {
+    let mut ans = 0;
+    for i in 1..=n {
+        if i % 2 == 0 {
+            continue;
+        }
+        let mut tmp = 0;
+        for j in 1..=((i as f64).sqrt()).ceil() as usize {
+            if i % j == 0 {
+                tmp += 1;
+                if j * j != i {
+                    tmp += 1;
+                }
+            }
+        }
+        if tmp == 8 {
+            ans += 1;
+        }
+    }
+    ans
+}
