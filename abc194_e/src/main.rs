d342dfe19bf42243ca6231c0222c890146f1a70e
dekokun <dekokun@hatena.ne.jp>
2021-03-06 22:54:57 +0900

    abc194_e pass

--- abc194_e/src/main.rs
+++ abc194_e/src/main.rs
@@ -1,4 +1,4 @@
-#![allow(non_snake_case, unused_macros)]
+#![allow(non_snake_case, unused_macros, dead_code)]
 
 // https://maguro.dev/debug-macro/ $B$+$i(B
 macro_rules! debug {
@@ -8,8 +8,11 @@ macro_rules! debug {
     };
 }
 
-use std::io::{stdin, Read};
 use std::str::FromStr;
+use std::{
+    collections::BTreeSet,
+    io::{stdin, Read},
+};
 fn read_option<T: FromStr>() -> Option<T> {
     let stdin = stdin();
     let stdin = stdin.lock();
@@ -28,7 +31,36 @@ fn read<T: FromStr>() -> T {
 
 fn main() {
     let n: usize = read();
-    println!("{}", solve(n));
+    let m = read();
+    println!("{}", solve(m, (0..n).map(|_| read()).collect()));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(m: usize, a: Vec<usize>) -> usize {
+    let mut set = BTreeSet::new();
+    for i in 0..=a.len() {
+        set.insert(i);
+    }
+    let mut count = 0;
+    let mut ans = a.len();
+    let mut counts = vec![0; a.len() + 1];
+    for (i, &v) in a.iter().enumerate() {
+        set.remove(&v);
+        counts[v] += 1;
+        if count < m {
+            count += 1;
+            if count == m {
+                ans = ans.min(*set.iter().next().unwrap());
+                debug!(set, v, ans, "=m");
+            }
+            continue;
+        }
+        // m個溜まってる状態
+        counts[a[i - m]] -= 1;
+        if counts[a[i - m]] == 0 {
+            set.insert(a[i - m]);
+        }
+        ans = ans.min(*set.iter().next().unwrap());
+        debug!(set, v, ans, ">m");
+    }
+    ans
+}
