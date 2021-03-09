14914b27d390e1e299aaec35f0a50dfa95f628a9
dekokun <dekokun@hatena.ne.jp>
2021-03-09 21:08:36 +0900

    abc120_d pass

--- abc120_d/src/main.rs
+++ abc120_d/src/main.rs
@@ -26,9 +26,135 @@ fn read<T: FromStr>() -> T {
     opt.expect("failed to parse token")
 }
 
+//https://github.com/rust-lang-ja/ac-library-rs
+
+pub mod dsu {
+    /// Implement (union by size) + (path compression)
+    /// Reference:
+    /// Zvi Galil and Giuseppe F. Italiano,
+    /// Data structures and algorithms for disjoint set union problems
+    pub struct Dsu {
+        n: usize,
+        // root node: -1 * component size
+        // otherwise: parent
+        parent_or_size: Vec<i32>,
+    }
+
+    impl Dsu {
+        // 0 <= size <= 10^8 is constrained.
+        pub fn new(size: usize) -> Self {
+            Self {
+                n: size,
+                parent_or_size: vec![-1; size],
+            }
+        }
+        pub fn merge(&mut self, a: usize, b: usize) -> usize {
+            assert!(a < self.n);
+            assert!(b < self.n);
+            let (mut x, mut y) = (self.leader(a), self.leader(b));
+            if x == y {
+                return x;
+            }
+            if -self.parent_or_size[x] < -self.parent_or_size[y] {
+                std::mem::swap(&mut x, &mut y);
+            }
+            self.parent_or_size[x] += self.parent_or_size[y];
+            self.parent_or_size[y] = x as i32;
+            x
+        }
+
+        pub fn same(&mut self, a: usize, b: usize) -> bool {
+            assert!(a < self.n);
+            assert!(b < self.n);
+            self.leader(a) == self.leader(b)
+        }
+        pub fn leader(&mut self, a: usize) -> usize {
+            assert!(a < self.n);
+            if self.parent_or_size[a] < 0 {
+                return a;
+            }
+            self.parent_or_size[a] = self.leader(self.parent_or_size[a] as usize) as i32;
+            self.parent_or_size[a] as usize
+        }
+        pub fn size(&mut self, a: usize) -> usize {
+            assert!(a < self.n);
+            let x = self.leader(a);
+            -self.parent_or_size[x] as usize
+        }
+        pub fn groups(&mut self) -> Vec<Vec<usize>> {
+            let mut leader_buf = vec![0; self.n];
+            let mut group_size = vec![0; self.n];
+            for i in 0..self.n {
+                leader_buf[i] = self.leader(i);
+                group_size[leader_buf[i]] += 1;
+            }
+            let mut result = vec![Vec::new(); self.n];
+            for i in 0..self.n {
+                result[i].reserve(group_size[i]);
+            }
+            for i in 0..self.n {
+                result[leader_buf[i]].push(i);
+            }
+            result
+                .into_iter()
+                .filter(|x| !x.is_empty())
+                .collect::<Vec<Vec<usize>>>()
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn dsu_works() {
+            let mut d = Dsu::new(4);
+            d.merge(0, 1);
+            assert_eq!(d.same(0, 1), true);
+            d.merge(1, 2);
+            assert_eq!(d.same(0, 2), true);
+            assert_eq!(d.size(0), 3);
+            assert_eq!(d.same(0, 3), false);
+            assert_eq!(d.groups(), vec![vec![0, 1, 2], vec![3]]);
+        }
+    }
+}
+use dsu::*;
+
 fn main() {
     let n: usize = read();
-    println!("{}", solve(n));
+    let m = read();
+
+    println!(
+        "{}",
+        solve(n, m, (0..m).map(|_| (read(), read())).collect())
+            .into_iter()
+            .map(|v| v.to_string())
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(n: usize, _m: usize, mut ab: Vec<(usize, usize)>) -> Vec<u64> {
+    ab.reverse();
+    let mut count = n as u64 * (n - 1) as u64 / 2;
+    let mut ans = vec![];
+    let mut dsu = Dsu::new(n);
+    for (a, b) in ab {
+        let a = a - 1;
+        let b = b - 1;
+        ans.push(count);
+        if dsu.same(a, b) {
+            continue;
+        }
+        let a_size = dsu.size(a);
+        let b_size = dsu.size(b);
+        count += a_size as u64 * (a_size - 1) as u64 / 2;
+        count += b_size as u64 * (b_size - 1) as u64 / 2;
+        dsu.merge(a, b);
+        let after_size = dsu.size(a);
+        count -= after_size as u64 * (after_size - 1) as u64 / 2;
+    }
+    ans.reverse();
+    ans
+}
