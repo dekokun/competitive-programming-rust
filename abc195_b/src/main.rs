ad6db4b107946b87b714924030e5029679aabbe4
dekokun <dekokun@hatena.ne.jp>
2021-03-13 21:14:17 +0900

    abc195_b pass

--- abc195_b/src/main.rs
+++ abc195_b/src/main.rs
@@ -12,9 +12,31 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        a: usize,
+        b: usize,
+        w: usize,
     }
-    println!("{}", solve(n));
+    println!("{}", solve(a, b, w));
+
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(a: usize, b: usize, w: usize) -> String {
+    let w = w * 1000;
+    let mut none = true;
+    let mut ans = (0, 0);
+    for i in 1..=w * 100 {
+        if none && a * i <= w && b * i >= w {
+            none = false;
+            ans.0 = i;
+        }
+        if !none && a * i > w {
+            ans.1 = i - 1;
+            break;
+        }
+    }
+    if ans == (0, 0) {
+        "UNSATISFIABLE".into()
+    } else {
+        format!("{} {}", ans.0, ans.1)
+    }
+}
