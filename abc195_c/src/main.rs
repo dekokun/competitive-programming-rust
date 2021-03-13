42dae6dab3e26637ff646790a7abb03633b803fa
dekokun <dekokun@hatena.ne.jp>
2021-03-13 21:14:20 +0900

    abc195_c initialize

---
+++ abc195_c/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(non_snake_case, unused_macros, dead_code)]
+
+use proconio::input;
+
+// https://maguro.dev/debug-macro/ から
+macro_rules! debug {
+    ($($a:expr),* $(,)*) => {
+        #[cfg(debug_assertions)]
+        eprintln!(concat!($("| ", stringify!($a), "={:?} "),*, "|"), $(&$a),*);
+    };
+}
+
+fn main() {
+    input! {
+        n: usize,
+    }
+    println!("{}", solve(n));
+}
+
+fn solve(n: usize) -> usize {}
