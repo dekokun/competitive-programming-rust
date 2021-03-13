710b88c8a203bb09f0836759dce33251645e6600
dekokun <dekokun@hatena.ne.jp>
2021-03-13 21:52:02 +0900

    abc195_d pass

--- abc195_d/src/main.rs
+++ abc195_d/src/main.rs
@@ -13,8 +13,56 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        q: usize,
+        wv: [(usize, usize); n],
+        x: [usize; m],
+        query: [(usize, usize); q],
     }
-    println!("{}", solve(n));
+    println!(
+        "{}",
+        solve(n, m, wv, x, query)
+            .into_iter()
+            .map(|v| v.to_string())
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(
+    _n: usize,
+    _m: usize,
+    mut wv: Vec<(usize, usize)>,
+    x: Vec<usize>,
+    query: Vec<(usize, usize)>,
+) -> Vec<usize> {
+    let mut ans = vec![];
+    wv.sort_by_key(|v| -(v.1 as i64));
+    for (l, r) in query {
+        let mut boxes = vec![];
+        for (i, &w) in x.iter().enumerate() {
+            if l - 1 <= i && i <= r - 1 {
+                continue;
+            }
+            boxes.push(w);
+        }
+        boxes.sort();
+        let mut sum = 0;
+        for &(w, v) in &wv {
+            let mut index = None;
+            for (i, &b) in boxes.iter().enumerate() {
+                if b >= w {
+                    sum += v;
+                    index = Some(i);
+                    break;
+                }
+            }
+            if let Some(i) = index {
+                boxes.remove(i);
+            }
+        }
+        ans.push(sum);
+    }
+
+    ans
+}
