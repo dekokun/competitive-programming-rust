a2bb36055b381bd6f3e98919fd5bc61e3b092593
dekokun <dekokun@hatena.ne.jp>
2021-03-28 21:12:48 +0900

    arc116_a pass

--- arc116_a/src/main.rs
+++ arc116_a/src/main.rs
@@ -1,7 +1,5 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
-use proconio::input;
-
 // https://maguro.dev/debug-macro/ から
 macro_rules! debug {
     ($($a:expr),* $(,)*) => {
@@ -10,11 +8,37 @@ macro_rules! debug {
     };
 }
 
-fn main() {
-    input! {
-        n: usize,
-    }
-    println!("{}", solve(n));
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
 }
 
-fn solve(n: usize) -> usize {}
+fn main() {
+    let t: usize = read();
+    for _ in 0..t {
+        println!("{}", solve(read()));
+    }
+}
+
+fn solve(n: usize) -> String {
+    if n % 2 == 1 {
+        return "Odd".into();
+    }
+    if (n / 2) % 2 == 1 {
+        return "Same".into();
+    }
+    "Even".into()
+}
