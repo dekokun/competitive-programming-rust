60d18c76ada53ceaa6d48e844793e2963d560d56
dekokun <dekokun@hatena.ne.jp>
2021-03-28 23:34:19 +0900

    increase in speed

--- runtest_func.sh
+++ runtest_func.sh
@@ -3,7 +3,11 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 runtest() {
-  output=$(echo "$1" | time cargo run -q)
+  exec_file=./target/debug/$(cd "$(dirname "$0")" && basename "$(pwd)")
+  if [ ! -e "$exec_file" ]; then
+    cargo build
+  fi
+  output=$(echo "$1" | time $exec_file)
   if [ "$?" -ne 0 ]; then
     echo -e "${RED}command fail${NC}"
     echo -e "please run command: echo $1 | time cargo run"
