74623eaf306d56cd328d1858edcdbd145d075dca
dekokun <dekokun@hatena.ne.jp>
2021-04-19 20:47:09 +0900

    arc117_a pass

--- arc117_a/src/main.rs
+++ arc117_a/src/main.rs
@@ -12,9 +12,38 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        a: usize,
+        b: usize
     }
-    println!("{}", solve(n));
+    println!(
+        "{}",
+        solve(a, b)
+            .into_iter()
+            .map(|a| a.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(a: usize, b: usize) -> Vec<i64> {
+    let mut ans = vec![];
+    let (max, min) = (a.max(b), a.min(b));
+    let mut max_sum = 0;
+    let mut min_sum = 0;
+    for i in 1..=max {
+        ans.push(i as i64);
+        max_sum += i;
+    }
+    for i in 1..=min - 1 {
+        ans.push(-(i as i64));
+        min_sum += i;
+    }
+    debug!(max_sum, min_sum);
+    ans.push(((max_sum - min_sum) as i64) * -1);
+    let ans = if b > a {
+        ans.into_iter().map(|a| -a).collect()
+    } else {
+        ans
+    };
+    ans
+}
