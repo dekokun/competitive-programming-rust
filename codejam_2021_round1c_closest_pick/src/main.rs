b265c659e26358aae8542fec003488f7893ec183
dekokun <dekokun@hatena.ne.jp>
2021-05-01 18:41:30 +0900

    codejam_2021_round1c_closest_pick pass

--- codejam_2021_round1c_closest_pick/src/main.rs
+++ codejam_2021_round1c_closest_pick/src/main.rs
@@ -8,16 +8,62 @@ macro_rules! debug {
     };
 }
 
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
+}
+
 fn main() {
     let T: usize = read();
     for t in 1..=T {
-        let m: usize = read();
+        let n: usize = read();
         println!(
             "Case #{}: {}",
             t,
-            solve((0..m).map(|_| (read(), read())).collect())
+            solve(read(), (0..n).map(|_| read()).collect())
         );
     }
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(k: usize, mut p: Vec<usize>) -> f64 {
+    p.sort();
+    let mut counts = vec![0];
+    let mut now = &p[0];
+    for i in &p[1..] {
+        if i - now <= 1 {
+            now = i;
+            continue;
+        }
+        counts.push(i - now - 1);
+        now = i;
+    }
+    let front_point = p[0] - 1;
+    let last_point = k - p[p.len() - 1];
+    // 1個ずつ別々に取る場合
+    let mut counts2 = vec![];
+    for i in &counts {
+        counts2.push((i + 1) / 2);
+    }
+    counts2.push(front_point);
+    counts2.push(last_point);
+    counts2.sort();
+    counts2.reverse();
+    let sum1 = counts2[0] + counts2[1];
+    // 2個入れる場合
+    let sum2 = counts.into_iter().max().unwrap();
+
+    std::cmp::max(sum1, sum2) as f64 / k as f64
+}
