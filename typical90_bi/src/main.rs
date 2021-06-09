0ef900dfe1202d87f335e71f39f85b6092a51333
dekokun <dekokun@hatena.ne.jp>
2021-06-09 22:04:33 +0900

    typical90_bi pass

--- typical90_bi/src/main.rs
+++ typical90_bi/src/main.rs
@@ -1,7 +1,5 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
-use proconio::input;
-
 // https://maguro.dev/debug-macro/ から
 macro_rules! debug {
     ($($a:expr),* $(,)*) => {
@@ -9,12 +7,37 @@ macro_rules! debug {
         eprintln!(concat!($("| ", stringify!($a), "={:?} "),*, "|"), $(&$a),*);
     };
 }
-
-fn main() {
-    input! {
-        n: usize,
-    }
-    println!("{}", solve(n));
+use std::collections::VecDeque;
+use std::io::{stdin, Read};
+use std::str::FromStr;
+fn read_option<T: FromStr>() -> Option<T> {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok()
+}
+fn read<T: FromStr>() -> T {
+    let opt = read_option();
+    opt.expect("failed to parse token")
 }
 
-fn solve(n: usize) -> usize {}
+fn main() {
+    let q: usize = read();
+    let mut deq = VecDeque::new();
+
+    for _ in 0..q {
+        let t: usize = read();
+        let x: usize = read();
+        match t {
+            1 => deq.push_front(x),
+            2 => deq.push_back(x),
+            3 => println!("{}", deq.get(x - 1).unwrap()),
+            _ => panic!(),
+        }
+    }
+}
