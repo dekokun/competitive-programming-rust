7be3cc6b1f5e6b3015e5f8b9af5e88dd50bb43d3
dekokun <dekokun@hatena.ne.jp>
2021-07-11 19:22:59 +0900

    typical90_d pass

--- typical90_d/src/main.rs
+++ typical90_d/src/main.rs
@@ -12,9 +12,49 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        h: usize,
+        w: usize,
+        map: [[usize; w]; h],
     }
-    println!("{}", solve(n));
+    println!(
+        "{}",
+        solve(h, w, map)
+            .into_iter()
+            .map(|vec| vec
+                .into_iter()
+                .map(|a| a.to_string())
+                .collect::<Vec<_>>()
+                .join(" "))
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(h: usize, w: usize, map: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let mut row_sums = vec![];
+    for i in 0..h {
+        let mut sum = 0;
+        for j in 0..w {
+            sum += map[i][j];
+        }
+        row_sums.push(sum);
+    }
+    let mut column_sums = vec![];
+    for i in 0..w {
+        let mut sum = 0;
+        for j in 0..h {
+            sum += map[j][i];
+        }
+        column_sums.push(sum);
+    }
+    debug!(row_sums, column_sums);
+    let mut ans = vec![];
+    for i in 0..h {
+        let mut row = vec![];
+        for j in 0..w {
+            row.push(row_sums[i] + column_sums[j] - map[i][j])
+        }
+        ans.push(row);
+    }
+    ans
+}
