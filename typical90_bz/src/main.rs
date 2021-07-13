a891b73c95f7c7461a1b9de078b8b842478985a1
dekokun <dekokun@hatena.ne.jp>
2021-07-13 13:46:40 +0900

    typical90_bz pass

--- typical90_bz/src/main.rs
+++ typical90_bz/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
+use std::collections::HashMap;
+
 use proconio::input;
 
 // https://maguro.dev/debug-macro/ から
@@ -13,8 +15,34 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
     }
-    println!("{}", solve(n));
+    println!("{}", solve(n, m, ab));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(_n: usize, _m: usize, ab: Vec<(usize, usize)>) -> usize {
+    let mut graph = HashMap::new();
+    for (a, b) in ab {
+        let entry = graph.entry(a).or_insert_with(|| vec![]);
+        entry.push(b);
+        let entry = graph.entry(b).or_insert_with(|| vec![]);
+        entry.push(a);
+    }
+    let mut ans = 0;
+    'outer: for (k, v) in graph {
+        let mut count = 0;
+        for i in v {
+            if i < k {
+                count += 1;
+            }
+            if count > 1 {
+                continue 'outer;
+            }
+        }
+        if count == 1 {
+            ans += 1;
+        }
+    }
+    ans
+}
