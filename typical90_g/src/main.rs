bd35b7d147f4ab331296ca68409f4b175b11a554
dekokun <dekokun@hatena.ne.jp>
2021-07-14 22:41:11 +0900

    typical90_g pass

--- typical90_g/src/main.rs
+++ typical90_g/src/main.rs
@@ -13,8 +13,42 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        a: [usize; n],
+        q: usize,
+        b: [usize; q],
     }
-    println!("{}", solve(n));
+    println!(
+        "{}",
+        solve(a, b)
+            .into_iter()
+            .map(|a| a.to_string())
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(mut a: Vec<usize>, b: Vec<usize>) -> Vec<usize> {
+    a.sort();
+    debug!(a);
+    let mut ans = vec![];
+    for v in b {
+        let mut l = -1;
+        let mut r = a.len() as i32;
+        while r - l > 1 {
+            let m = (r + l) / 2;
+            if v < a[m as usize] {
+                r = m;
+            } else {
+                l = m;
+            }
+        }
+        ans.push(if r == a.len() as i32 {
+            v - a[a.len() - 1]
+        } else if l == -1 {
+            a[0] - v
+        } else {
+            (a[r as usize] - v).min(v - a[l as usize])
+        });
+    }
+    ans
+}
