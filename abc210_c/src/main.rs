f4818d5f8dbd98ef765e1e666b257158d203a7f7
dekokun <dekokun@hatena.ne.jp>
2021-07-17 21:33:26 +0900

    local pass

--- abc210_c/src/main.rs
+++ abc210_c/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
+use std::collections::HashMap;
+
 use proconio::input;
 
 // https://maguro.dev/debug-macro/ から
@@ -13,8 +15,34 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        k: usize,
+        c: [usize; n],
     }
-    println!("{}", solve(n));
+    println!("{}", solve(n, k, c));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(_n: usize, k: usize, c: Vec<usize>) -> usize {
+    let mut ans = 0;
+    let mut map = HashMap::new();
+    for (i, &color) in c.iter().enumerate() {
+        let entry = map.entry(color).or_insert(0);
+        *entry += 1;
+        if i < k {
+            ans = ans.max(map.len());
+            continue;
+        }
+        match map.get(&c[i - k]) {
+            Some(1) => {
+                map.remove(&c[i - k]);
+            }
+            Some(0) => panic!(),
+            Some(&a) => {
+                map.insert(c[i - k], a - 1);
+            }
+            _ => panic!(),
+        }
+        debug!(map, ans);
+        ans = ans.max(map.len())
+    }
+    ans
+}
