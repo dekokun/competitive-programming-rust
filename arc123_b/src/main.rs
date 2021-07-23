324e465b6bfcdc606007143ddd99416a7a78330a
dekokun <dekokun@hatena.ne.jp>
2021-07-23 21:44:27 +0900

    tmp

--- arc123_b/src/main.rs
+++ arc123_b/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
+use std::collections::BinaryHeap;
+
 use proconio::input;
 
 // https://maguro.dev/debug-macro/ から
@@ -13,8 +15,31 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        a: [usize; n],
+        b: [usize; n],
+        c: [usize; n],
     }
-    println!("{}", solve(n));
+    println!("{}", solve(a, b, c));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(a: Vec<usize>, b: Vec<usize>, c: Vec<usize>) -> usize {
+    let mut a: BinaryHeap<usize> = a.into();
+    let mut b: BinaryHeap<usize> = b.into();
+    let mut c: BinaryHeap<usize> = c.into();
+    let mut ans = 0;
+    'outer: while let Some(c) = c.pop() {
+        while let Some(b) = b.pop() {
+            if b >= c {
+                continue;
+            }
+            while let Some(a) = a.pop() {
+                if a >= b {
+                    continue;
+                }
+                ans += 1;
+                continue 'outer;
+            }
+        }
+    }
+    ans
+}
