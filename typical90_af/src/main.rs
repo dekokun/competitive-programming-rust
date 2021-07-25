c573c9282da68fb6f4754a893c4253700aa7172b
dekokun <dekokun@hatena.ne.jp>
2021-07-25 16:47:07 +0900

    typical90_af pass

--- typical90_af/src/main.rs
+++ typical90_af/src/main.rs
@@ -1,6 +1,9 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
+use itertools::Itertools;
+
 use proconio::input;
+use proconio::marker::Usize1;
 
 // https://maguro.dev/debug-macro/ から
 macro_rules! debug {
@@ -13,8 +16,39 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        a: [[i32; n]; n],
+        m: usize,
+        x: [(Usize1, Usize1); m],
     }
-    println!("{}", solve(n));
+    println!("{}", solve(n, a, x));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(n: usize, a: Vec<Vec<i32>>, x: Vec<(usize, usize)>) -> i32 {
+    let mut m = vec![vec![false; n]; n];
+    for (x, y) in x {
+        m[x][y] = true;
+        m[y][x] = true;
+    }
+    let max = 1000000;
+    let mut ans = max;
+    'outer: for perm in (0..n).permutations(n) {
+        debug!(perm);
+        for v in perm.windows(2) {
+            if m[v[0]][v[1]] {
+                debug!("skipped");
+                continue 'outer;
+            }
+        }
+        let mut tmp = 0;
+        for (i, v) in perm.into_iter().enumerate() {
+            tmp += a[v][i];
+        }
+        ans = ans.min(tmp);
+        debug!(tmp, ans);
+    }
+    if ans == max {
+        -1
+    } else {
+        ans
+    }
+}
