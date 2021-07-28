afbbbfcdcd69ea025fa285ddf68049c0e6f81ad1
dekokun <dekokun@hatena.ne.jp>
2021-07-28 09:01:06 +0900

    typical90_at pass

--- typical90_at/src/main.rs
+++ typical90_at/src/main.rs
@@ -13,8 +13,35 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        a: [usize; n],
+        b: [usize; n],
+        c: [usize; n],
     }
-    println!("{}", solve(n));
+    println!("{}", solve(a, b, c));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(a: Vec<usize>, b: Vec<usize>, c: Vec<usize>) -> u64 {
+    let mut ma = vec![0; 46];
+    for v in a {
+        ma[v % 46] += 1;
+    }
+    let mut mb = vec![0; 46];
+    for v in b {
+        mb[v % 46] += 1;
+    }
+    let mut mc = vec![0; 46];
+    for v in c {
+        mc[v % 46] += 1;
+    }
+    let mut ans = 0;
+    for i in 0..46 {
+        for j in 0..46 {
+            for k in 0..46 {
+                if (i + j + k) % 46 == 0 {
+                    ans += ma[i] * mb[j] * mc[k];
+                }
+            }
+        }
+    }
+    ans
+}
