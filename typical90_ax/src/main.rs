bc4ce7ef0649e1e63ef1a222408d6ecd5e830870
dekokun <dekokun@hatena.ne.jp>
2021-07-30 21:41:22 +0900

    typical90_ax pass

--- typical90_ax/src/main.rs
+++ typical90_ax/src/main.rs
@@ -13,8 +13,21 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        l: usize,
     }
-    println!("{}", solve(n));
+    println!("{}", solve(n, l));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(n: usize, l: usize) -> usize {
+    let MOD = 10_usize.pow(9) + 7;
+    let mut dp = vec![0; n + l + 100];
+    dp[0] = 1;
+    for i in 0..=n {
+        let now = dp[i];
+        dp[i + 1] += now;
+        dp[i + 1] %= MOD;
+        dp[i + l] += now;
+        dp[i + l] %= MOD;
+    }
+    dp[n]
+}
