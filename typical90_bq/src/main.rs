09868c44e2de5f421193663fefba123028f709ad
dekokun <dekokun@hatena.ne.jp>
2021-08-01 21:05:44 +0900

    typical90_bq pass

--- typical90_bq/src/main.rs
+++ typical90_bq/src/main.rs
@@ -12,9 +12,33 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        n: i64,
+        k: i64,
     }
-    println!("{}", solve(n));
+    println!("{}", solve(n, k));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(n: i64, k: i64) -> i64 {
+    let MOD = 10_i64.pow(9) + 7;
+    if n == 1 {
+        return k;
+    }
+    if n == 2 {
+        return k * (k - 1);
+    }
+    ((k * (k - 1)) % MOD * mod_pow(k - 2, n - 2, MOD)) % MOD
+}
+
+fn mod_pow(x: i64, n: i64, MOD: i64) -> i64 {
+    let mut ans = 1;
+    let mut n = n;
+    let mut x = x;
+    while n != 0 {
+        if n % 2 == 1 {
+            ans = (ans * x) % MOD;
+        }
+        x = (x * x) % MOD;
+        n /= 2;
+    }
+    ans
+}
