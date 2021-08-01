ce6fc2d1d24438749fb5379f5fa5da7939690e10
dekokun <dekokun@hatena.ne.jp>
2021-08-01 21:42:30 +0900

    typical90_bw pass

--- typical90_bw/src/main.rs
+++ typical90_bw/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
+use std::collections::HashMap;
+
 use proconio::input;
 
 // https://maguro.dev/debug-macro/ から
@@ -12,9 +14,36 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        n: u64,
     }
     println!("{}", solve(n));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(n: u64) -> usize {
+    let primes = prime_factorization(n);
+    let prime_count = primes.into_iter().fold(0, |acc, (_i, v)| acc + v);
+    let mut now = prime_count;
+    let mut ans = 0;
+    while now != 1 {
+        ans += 1;
+        now = (now + 1) / 2;
+    }
+    ans
+}
+
+fn prime_factorization(n: u64) -> HashMap<u64, usize> {
+    let mut ans = HashMap::new();
+    let mut n = n;
+    for i in 2..=((n as f64).sqrt().ceil() as u64) {
+        while n % i == 0 && n != 1 {
+            n /= i;
+            let entry = ans.entry(i).or_insert(0);
+            *entry += 1;
+        }
+    }
+    if n != 1 {
+        let entry = ans.entry(n).or_insert(0);
+        *entry += 1;
+    }
+    ans
+}
