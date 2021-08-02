a14de392b0666c4252fe9e12b34fd7a25f45d19c
dekokun <dekokun@hatena.ne.jp>
2021-08-02 14:04:38 +0900

    typical90_ca pass

--- typical90_ca/src/main.rs
+++ typical90_ca/src/main.rs
@@ -12,9 +12,51 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        h: usize,
+        w: usize,
+        a: [[isize; w]; h],
+        b: [[isize; w]; h],
     }
-    println!("{}", solve(n));
+    println!(
+        "{}",
+        match solve(a, b) {
+            None => "No".into(),
+            Some(a) => format!("Yes\n{}", a),
+        }
+    );
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(mut a: Vec<Vec<isize>>, b: Vec<Vec<isize>>) -> Option<isize> {
+    let mut ans = 0;
+    for i in 0..a.len() - 1 {
+        for j in 0..a[0].len() - 1 {
+            if a[i][j] == b[i][j] {
+                continue;
+            }
+            if a[i][j] > b[i][j] {
+                let diff = a[i][j] - b[i][j];
+                ans += diff;
+                a[i][j] -= diff;
+                a[i + 1][j] -= diff;
+                a[i][j + 1] -= diff;
+                a[i + 1][j + 1] -= diff;
+            }
+            if a[i][j] < b[i][j] {
+                let diff = b[i][j] - a[i][j];
+                ans += diff;
+                a[i][j] += diff;
+                a[i + 1][j] += diff;
+                a[i][j + 1] += diff;
+                a[i + 1][j + 1] += diff;
+            }
+        }
+    }
+    for i in 0..a.len() {
+        for j in 0..a[0].len() {
+            if a[i][j] != b[i][j] {
+                return None;
+            }
+        }
+    }
+    Some(ans)
+}
