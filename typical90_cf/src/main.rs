39a70bbfb4b4b0ef75677f90dfd33efe6a8b6ab2
dekokun <dekokun@hatena.ne.jp>
2021-08-17 22:11:05 +0900

    typical90_cf pass

--- typical90_cf/src/main.rs
+++ typical90_cf/src/main.rs
@@ -19,7 +19,61 @@ fn main() {
 }
 
 fn solve(n: usize, s: String) -> usize {
-    let mut map = vec![None; n];
-    let mut last = None;
-    for c in s.chars().enumerate().collect::<Vec<_>>().windows(2) {}
+    let next_x = s
+        .chars()
+        .rev()
+        .enumerate()
+        .fold(vec![], |mut acc, (i, c)| {
+            if c == 'x' {
+                acc.push(Some(n - i - 1));
+                acc
+            } else if acc.is_empty() {
+                vec![None]
+            } else {
+                acc.push(acc[acc.len() - 1]);
+                acc
+            }
+        })
+        .into_iter()
+        .rev()
+        .collect::<Vec<_>>();
+    let next_o = s
+        .chars()
+        .rev()
+        .enumerate()
+        .fold(vec![], |mut acc, (i, c)| {
+            if c == 'o' {
+                acc.push(Some(n - i - 1));
+                acc
+            } else if acc.is_empty() {
+                vec![None]
+            } else {
+                acc.push(acc[acc.len() - 1]);
+                acc
+            }
+        })
+        .into_iter()
+        .rev()
+        .collect::<Vec<_>>();
+    let mut ans = 0;
+    for (i, c) in s.chars().enumerate() {
+        ans += if c == 'x' {
+            let next = next_o[i];
+            if next.is_none() {
+                0
+            } else {
+                let next = next.unwrap();
+                n - next
+            }
+        } else {
+            let next = next_x[i];
+            if next.is_none() {
+                0
+            } else {
+                let next = next.unwrap();
+                n - next
+            }
+        }
+    }
+    ans
 }
