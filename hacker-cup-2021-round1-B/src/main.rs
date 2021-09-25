e0184f1ec94ee7cd6af573cc32937c2f0f349b0f
dekokun <dekokun@hatena.ne.jp>
2021-09-26 01:56:42 +0900

    hacker-cup-2021-round1-B pass

--- hacker-cup-2021-round1-B/src/main.rs
+++ hacker-cup-2021-round1-B/src/main.rs
@@ -12,9 +12,35 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        T: usize,
     }
-    println!("{}", solve(n));
+    for t in 1..=T {
+        input! {
+            n: usize,
+            m: usize,
+            a: usize,
+            b: usize
+        }
+        println!("Case #{}: {}", t, solve(n, m, a, b));
+    }
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(n: usize, m: usize, a: usize, b: usize) -> String {
+    if n + m - 1 > a || n + m - 1 > b {
+        return "Impossible".into();
+    }
+    let mut ans = vec![vec![1; m]; n];
+    ans[0][0] = a - (n + m - 2);
+    ans[0][m - 1] = b - (n + m - 2);
+    return "Possible\n".to_string()
+        + &ans
+            .into_iter()
+            .map(|v| {
+                v.into_iter()
+                    .map(|s| s.to_string())
+                    .collect::<Vec<_>>()
+                    .join(" ")
+            })
+            .collect::<Vec<_>>()
+            .join("\n");
+}
