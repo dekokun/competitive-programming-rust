7fc0ba67de65641614832ea1a271a682313c24ea
dekokun <dekokun@hatena.ne.jp>
2021-10-25 13:57:23 +0900

    abc224_b pass

--- abc224_b/src/main.rs
+++ abc224_b/src/main.rs
@@ -12,9 +12,24 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        h: usize,
+        w: usize,
+        map: [[usize; w]; h]
     }
-    println!("{}", solve(n));
+    println!("{}", solve(h, w, map));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(h: usize, w: usize, m: Vec<Vec<usize>>) -> String {
+    for i1 in 0..h {
+        for i2 in i1 + 1..h {
+            for j1 in 0..w {
+                for j2 in j1 + 1..w {
+                    if m[i1][j1] + m[i2][j2] > m[i2][j1] + m[i1][j2] {
+                        return "No".into();
+                    }
+                }
+            }
+        }
+    }
+    "Yes".into()
+}
