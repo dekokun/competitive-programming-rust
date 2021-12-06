8683a0e68f45ae12dfe7f25ef9b7de89d71f8189
dekokun <dekokun@hatena.ne.jp>
2021-12-06 14:01:52 +0900

    RE...

--- abc230_b/src/main.rs
+++ abc230_b/src/main.rs
@@ -12,9 +12,42 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        s: String,
     }
-    println!("{}", solve(n));
+    println!("{}", solve(s));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(s: String) -> String {
+    if s.len() == 0 {
+        return "Yes".into();
+    }
+    if s.len() == 2 {
+        match &s[0..] {
+            "ox" | "xx" | "xo" => return "Yes".into(),
+            _ => return "No".into(),
+        };
+    }
+
+    let s = match &s[0..2] {
+        "ox" => s,
+        "xx" => "o".to_owned() + &s,
+        "xo" => "ox".to_owned() + &s,
+        _ => return "No".into(),
+    };
+    let s = match &s[s.len() - 2..] {
+        "ox" => s + "x",
+        "xx" => s,
+        "xo" => s + "xx",
+        _ => return "No".into(),
+    };
+    debug!(s);
+    for i in 0..s.len() {
+        if i % 3 != 0 {
+            continue;
+        }
+        if &s[i..i + 3] != "oxx" {
+            return "No".into();
+        }
+    }
+    return "Yes".into();
+}
