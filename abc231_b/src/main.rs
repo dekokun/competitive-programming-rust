d3179183934f57201fe35f3d587e21d466351361
dekokun <dekokun@hatena.ne.jp>
2021-12-11 21:12:22 +0900

    abc231_b pass

--- abc231_b/src/main.rs
+++ abc231_b/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
+use std::collections::HashMap;
+
 use proconio::input;
 
 // https://maguro.dev/debug-macro/
@@ -13,8 +15,24 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        s: [String; n]
     }
-    println!("{}", solve(n));
+    println!("{}", solve(n, s));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(n: usize, s: Vec<String>) -> String {
+    let mut m = HashMap::new();
+    for name in s {
+        let entry = m.entry(name).or_insert(0);
+        *entry += 1;
+    }
+    let mut max = 0;
+    let mut ans = "".to_string();
+    for (name, v) in m {
+        if max <= v {
+            max = v;
+            ans = name;
+        }
+    }
+    ans.into()
+}
