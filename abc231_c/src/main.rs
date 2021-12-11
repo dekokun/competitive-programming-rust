ea5ae312accbdd9f2b55db705ea8b1b969426a49
dekokun <dekokun@hatena.ne.jp>
2021-12-11 21:34:19 +0900

    abc231_c pass

--- abc231_c/src/main.rs
+++ abc231_c/src/main.rs
@@ -13,8 +13,35 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        q: usize,
+        a: [usize; n],
+        x: [usize; q]
     }
-    println!("{}", solve(n));
+    println!(
+        "{}",
+        solve(a, x)
+            .into_iter()
+            .map(|a| a.to_string())
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(mut a: Vec<usize>, x: Vec<usize>) -> Vec<usize> {
+    a.sort();
+    let mut ans = vec![];
+    for i in x {
+        let res = a.binary_search(&i);
+        match res {
+            Ok(c) => {
+                let mut left = c;
+                while left != 0 && a[left - 1] == a[left] {
+                    left -= 1;
+                }
+                ans.push(a.len() - left);
+            }
+            Err(c) => ans.push(a.len() - c),
+        }
+    }
+    ans
+}
