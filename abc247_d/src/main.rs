fda33a8b041823576fcc6b8bca64bafe2e3ff62f
dekokun <dekokun@hatena.ne.jp>
2022-04-11 22:42:52 +0900

    abc247_d pass

--- abc247_d/src/main.rs
+++ abc247_d/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
+use std::collections::VecDeque;
+
 use proconio::input;
 
 // https://maguro.dev/debug-macro/
@@ -12,9 +14,60 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        q: usize,
     }
-    println!("{}", solve(n));
+    let mut qs = vec![];
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+        let query = if t == 1 {
+            input! {
+                query: [usize; 2],
+            }
+            query
+        } else {
+            input! {
+                query: [usize; 1],
+            }
+            query
+        };
+        qs.push(query);
+    }
+    println!(
+        "{}",
+        solve(qs)
+            .into_iter()
+            .map(|a| a.to_string())
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(q: Vec<Vec<usize>>) -> Vec<usize> {
+    let mut queue = VecDeque::new();
+    let mut ans = vec![];
+    for v in q {
+        if v.len() == 2 {
+            queue.push_back((v[0], v[1]))
+        } else {
+            let mut rem = v[0];
+            let mut sum = 0;
+            while let Some((x, c)) = queue.pop_front() {
+                if rem < c {
+                    queue.push_front((x, c - rem));
+                    sum += x * rem;
+                    break;
+                } else if rem == c {
+                    sum += x * rem;
+                    break;
+                } else {
+                    sum += x * c;
+                    rem -= c;
+                }
+            }
+            ans.push(sum);
+        };
+    }
+    ans
+}
