a9ed6e7fc28a4a136150e8df81c452a940a17645
dekokun <dekokun@hatena.ne.jp>
2022-04-18 10:12:13 +0900

    abc248_a pass

--- abc248_a/src/main.rs
+++ abc248_a/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 // https://maguro.dev/debug-macro/
 macro_rules! debug {
@@ -12,9 +12,17 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        s: Chars,
     }
-    println!("{}", solve(n));
+    println!("{}", solve(s));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(s: Vec<char>) -> u32 {
+    for i in 0..=9 {
+        if s.contains(&std::char::from_digit(i, 10).unwrap()) {
+            continue;
+        }
+        return i;
+    }
+    panic!()
+}
