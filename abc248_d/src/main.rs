2a5b320fa2de51c8b4a6d0c8d5af00d3cfd0ed9e
dekokun <dekokun@hatena.ne.jp>
2022-04-22 16:02:45 +0900

    abc248_d pass

--- abc248_d/src/main.rs
+++ abc248_d/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
-use proconio::input;
+use std::collections::HashMap;
+
+use proconio::{input, marker::Usize1};
 
 // https://maguro.dev/debug-macro/
 macro_rules! debug {
@@ -13,8 +15,43 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        a: [usize; n],
+        q: usize,
+        qs: [(Usize1, Usize1, usize); q]
     }
-    println!("{}", solve(n));
+    println!(
+        "{}",
+        solve(a, qs)
+            .into_iter()
+            .map(|a| a.to_string())
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(a: Vec<usize>, qs: Vec<(usize, usize, usize)>) -> Vec<usize> {
+    let mut m = HashMap::new();
+    for (i, v) in a.into_iter().enumerate() {
+        let e = m.entry(v).or_insert(vec![]);
+        e.push(i);
+    }
+    let mut ans = vec![];
+    for (l, r, x) in qs {
+        debug!(l, r, x);
+        if !m.contains_key(&x) {
+            ans.push(0);
+            continue;
+        }
+        let mut tmp = m[&x].len();
+        tmp -= match m[&x].binary_search(&l) {
+            Ok(a) => a,
+            Err(a) => a,
+        };
+        tmp -= match m[&x].binary_search(&r) {
+            Ok(a) => m[&x].len() - a - 1,
+            Err(a) => m[&x].len() - a,
+        };
+        ans.push(tmp);
+    }
+    ans
+}
