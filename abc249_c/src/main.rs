9a7e43d0570b02d19dd353a1f96b9e59c2b58267
dekokun <dekokun@hatena.ne.jp>
2022-04-23 21:42:08 +0900

    abc249_c pass

--- abc249_c/src/main.rs
+++ abc249_c/src/main.rs
@@ -1,5 +1,8 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
+use std::collections::HashMap;
+
+use itertools::Itertools;
 use proconio::input;
 
 // https://maguro.dev/debug-macro/
@@ -13,8 +16,31 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        k: usize,
+        s: [String; n]
     }
-    println!("{}", solve(n));
+    println!("{}", solve(k, s));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(k: usize, s: Vec<String>) -> usize {
+    let mut ans = 0;
+    for i in 1..=s.len() {
+        for ss in s.iter().combinations(i) {
+            let mut sums = HashMap::new();
+            for s in ss {
+                for c in s.chars() {
+                    let e = sums.entry(c).or_insert(0);
+                    *e += 1;
+                }
+            }
+            let mut tmp = 0;
+            for (_key, v) in sums {
+                if v == k {
+                    tmp += 1;
+                }
+            }
+            ans = ans.max(tmp);
+        }
+    }
+    ans
+}
