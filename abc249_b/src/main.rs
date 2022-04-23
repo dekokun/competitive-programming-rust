cd38dc51aa2f04a4c51af75ef8f2d66c68171a5a
dekokun <dekokun@hatena.ne.jp>
2022-04-23 21:28:00 +0900

    abc249_b pass

--- abc249_b/src/main.rs
+++ abc249_b/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
+use std::collections::HashSet;
+
 use proconio::input;
 
 // https://maguro.dev/debug-macro/
@@ -12,9 +14,31 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        s: String,
     }
-    println!("{}", solve(n));
+    println!("{}", solve(s));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(s: String) -> String {
+    let mut komoji_exist = false;
+    let mut omoji_exist = false;
+    let mut differ = true;
+    let mut sts = HashSet::new();
+    for c in s.chars() {
+        if c.is_uppercase() {
+            omoji_exist = true;
+        }
+        if c.is_lowercase() {
+            komoji_exist = true;
+        }
+        if sts.contains(&c) {
+            differ = false;
+        }
+        sts.insert(c);
+    }
+    if omoji_exist && komoji_exist && differ {
+        "Yes"
+    } else {
+        "No"
+    }.into()
+}
