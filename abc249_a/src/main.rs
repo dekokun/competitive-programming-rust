d9875a3fe0533d45926d5bd4cc7957f7a756a394
dekokun <dekokun@hatena.ne.jp>
2022-04-23 21:22:05 +0900

    abc249_a pass

--- abc249_a/src/main.rs
+++ abc249_a/src/main.rs
@@ -12,9 +12,36 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
+        e: usize,
+        f: usize,
+        x: usize,
     }
-    println!("{}", solve(n));
+    println!("{}", solve(a, b, c, d, e, f, x));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(a: usize, b: usize, c: usize, d: usize, e: usize, f: usize, x: usize) -> String {
+    let t_d = ((x / (a + c)) * a * b)
+        + if (x % (a + c)) >= a {
+            b * a
+        } else {
+            (x % (a + c)) * b
+        };
+    let a_d = ((x / (d + f)) * d * e)
+        + if (x % (d + f)) >= d {
+            e * d
+        } else {
+            (x % (d + f)) * e
+        };
+    if t_d == a_d {
+        "Draw"
+    } else if t_d > a_d {
+        "Takahashi"
+    } else {
+        "Aoki"
+    }
+    .into()
+}
