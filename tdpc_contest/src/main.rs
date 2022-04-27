5423244cfcd164b70521ecf0006849942bbab514
dekokun <dekokun@hatena.ne.jp>
2022-04-27 13:37:10 +0900

    tdpc_contest pass

--- tdpc_contest/src/main.rs
+++ tdpc_contest/src/main.rs
@@ -13,8 +13,32 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        p: [usize; n],
     }
-    println!("{}", solve(n));
+    println!("{}", solve(p));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(p: Vec<usize>) -> usize {
+    let max = p.iter().sum();
+    let mut dp = vec![vec![false; max + 101]; p.len() + 1];
+    dp[0][0] = true;
+    for i in 0..p.len() {
+        for j in 0..=max {
+            if !dp[i][j] {
+                continue;
+            }
+            // 正解
+            dp[i + 1][j + p[i]] = true;
+            // 不正解
+            dp[i + 1][j] = true;
+        }
+    }
+    let mut ans = 0;
+    debug!(dp);
+    for &v in &dp[p.len()] {
+        if v {
+            ans += 1;
+        }
+    }
+    ans
+}
