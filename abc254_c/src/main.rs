d6780d215b5f9d20bc9b1b80f3f334a319827f34
dekokun <dekokun@hatena.ne.jp>
2022-06-04 22:21:45 +0900

    abc254_c pass

--- abc254_c/src/main.rs
+++ abc254_c/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
+use std::collections::{HashMap, HashSet};
+
 use proconio::input;
 
 // https://maguro.dev/debug-macro/
@@ -13,8 +15,47 @@ macro_rules! debug {
 fn main() {
     input! {
         n: usize,
+        k: usize,
+        a: [usize; n]
     }
-    println!("{}", solve(n));
+    println!("{}", solve(k, a));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(k: usize, a: Vec<usize>) -> String {
+    if k == 1 {
+        return "Yes".into();
+    }
+    let mut b = a.clone();
+    b.sort();
+    let b = b.into_iter().enumerate().collect::<Vec<_>>();
+    let mut m = HashMap::new();
+    for (i, v) in b {
+        let e = m.entry(v).or_insert_with(|| (i, i));
+        e.1 = i;
+    }
+    let mut comp = HashSet::new();
+    for (i, v) in a.into_iter().enumerate() {
+        let &(min, max) = m.get(&v).unwrap();
+        let rem = i % k;
+        let mut hit = if min <= (min / k) * k + rem && (min / k) * k + rem <= max {
+            (min / k) * k + rem
+        } else if min <= (min / k) * k + rem + k && (min / k) * k + rem + k <= max {
+            (min / k) * k + rem + k
+        } else {
+            return "No".into();
+        };
+        if comp.contains(&hit) {
+            loop {
+                hit += k;
+                if hit <= max && !comp.contains(&hit) {
+                    break;
+                }
+                if hit > max {
+                    return "No".into();
+                }
+            }
+        }
+        comp.insert(hit);
+    }
+    "Yes".into()
+}
