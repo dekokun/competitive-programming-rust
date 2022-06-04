9b8386b66ed7521621615aa2eb3fb561cf807885
dekokun <dekokun@hatena.ne.jp>
2022-06-04 21:24:04 +0900

    abc254_b pass

--- abc254_b/src/main.rs
+++ abc254_b/src/main.rs
@@ -14,7 +14,32 @@ fn main() {
     input! {
         n: usize,
     }
-    println!("{}", solve(n));
+    println!("{}", solve(n).join("\n"));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(n: usize) -> Vec<String> {
+    let mut ans: Vec<Vec<usize>> = vec![];
+    for i in 0..n {
+        if i == 0 {
+            ans.push(vec![1]);
+            continue;
+        }
+        let mut tmp = vec![];
+        for j in 0..=i {
+            if j == 0 || j == i {
+                tmp.push(1);
+                continue;
+            }
+            tmp.push(ans[i - 1][j - 1] + ans[i - 1][j]);
+        }
+        ans.push(tmp);
+    }
+    ans.into_iter()
+        .map(|a| {
+            a.into_iter()
+                .map(|i| i.to_string())
+                .collect::<Vec<_>>()
+                .join(" ")
+        })
+        .collect()
+}
