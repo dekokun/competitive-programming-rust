c9c5209f9033668a77b9c8415ffd23240d90c6fc
dekokun <shintaro.kurachi@gmail.com>
2022-10-24 17:06:35 +0900

    abc274_b pass

--- abc274_b/src/main.rs
+++ abc274_b/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case, unused_macros, dead_code)]
 
 use proconio::input;
+use proconio::marker::Chars;
 
 // https://maguro.dev/debug-macro/
 macro_rules! debug {
@@ -12,9 +13,21 @@ macro_rules! debug {
 
 fn main() {
     input! {
-        n: usize,
+        h: usize,
+        w: usize,
+        c: [Chars; h]
     }
-    println!("{}", solve(n));
+    println!("{}", solve(h, w, c).into_iter().map(|a| a.to_string()).collect::<Vec<_>>().join(" "));
 }
 
-fn solve(n: usize) -> usize {}
+fn solve(_: usize, w: usize, c: Vec<Vec<char>>) -> Vec<usize> {
+    let mut ans = vec![0; w];
+    for v in c.iter() {
+        for (i, c) in v.into_iter().enumerate() {
+            if c == &'#' {
+                ans[i] += 1;
+            }
+        }
+    }
+    ans
+}
